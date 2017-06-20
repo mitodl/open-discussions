24fb8d8de2727b8b1864c7e6923880c910abe5ec
George Schneeloch <gschneel@mit.edu>
2017-06-20 11:47:44 -0400

    Fix missing style bundle (#8)

--- webpack.config.shared.js
+++ webpack.config.shared.js
@@ -5,6 +5,7 @@ module.exports = {
   config: {
     entry: {
       'root': ['babel-polyfill', './static/js/entry/root'],
+      'style': './static/js/entry/style',
     },
     module: {
       rules: [
