1f4ec7f972177c495a104e79450dc345595a5d9b
George Schneeloch <gschneel@mit.edu>
2017-07-27 17:36:37 -0400

    Add commons chunk for dev

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -24,6 +24,10 @@ const devConfig = Object.assign({}, config, {
         'NODE_ENV': '"development"'
       }
     }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'common',
+      minChunks: 2,
+    }),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NamedModulesPlugin(),
     new webpack.NoEmitOnErrorsPlugin(),
