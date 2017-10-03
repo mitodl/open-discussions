d908149a912a8b01fc3c2e9d8f03d9a13b5bac8e
Alice Pote <alice.writes.wrongs@gmail.com>
2017-10-03 12:03:11 -0400

    ok I have no idea why this isn't working

--- static/js/components/Analytics.js
+++ static/js/components/Analytics.js
@@ -3,12 +3,21 @@ import React from "react"
 import { withRouter } from "react-router"
 import ReactGA from 'react-ga'
 
+if (SETTINGS.gaTrackingID) {
+  console.log(SETTINGS.gaTrackingID);
+  ReactGA.initialize(SETTINGS.gaTrackingID, { debug: true })
+  console.log('initializing');
+}
+
 class Analytics extends React.Component {
   componentDidUpdate(prevProps) {
     const { location } = this.props
 
+    console.log('potato');
+
     if (location !== prevProps.location) {
-      ga.pageview(window.location.pathname)
+      console.log(window.location.pathname);
+      ReactGA.pageview(window.location.pathname)
     }
   }
 
