9027829ff612a94d6ba2da30e3943a6caf0dbc3a
George Schneeloch <gschneel@mit.edu>
2017-10-11 12:41:52 -0400

    Fix root logger location (#266)

--- open_discussions/settings.py
+++ open_discussions/settings.py
@@ -278,10 +278,6 @@ LOGGING = {
         },
     },
     'loggers': {
-        'root': {
-            'handlers': ['console', 'syslog'],
-            'level': LOG_LEVEL,
-        },
         'django': {
             'propagate': True,
             'level': DJANGO_LOG_LEVEL,
@@ -300,6 +296,10 @@ LOGGING = {
             'handlers': ['console'],
             'level': 'ERROR',
         }
+    },
+    'root': {
+        'handlers': ['console', 'syslog'],
+        'level': LOG_LEVEL,
     },
 }
 
