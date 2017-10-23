06e42cea43f58a769a0ab66b673ec5c13717e4db
Alice Pote <alice.writes.wrongs@gmail.com>
2017-10-23 12:34:27 -0400

    Add https to placeholder
    The placeholder text for the link post creation form was a little
    misleading before, this fixes it.
    
    closes #297
    pr #306

--- static/js/components/CreatePostForm.js
+++ static/js/components/CreatePostForm.js
@@ -108,7 +108,7 @@ export default class CreatePostForm extends React.Component {
                 <label>Link URL</label>
                 <input
                   type="url"
-                  placeholder="www.example.com"
+                  placeholder="https://www.example.com"
                   name="url"
                   value={url}
                   onChange={onUpdate}
