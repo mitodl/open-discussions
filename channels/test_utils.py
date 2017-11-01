b3c56e13100a6462264eb20d6fa9374a7ecaee9f
George Schneeloch <gschneel@mit.edu>
2017-11-01 13:49:25 -0400

    Use try/finally in context managers (#311)

--- channels/test_utils.py
+++ channels/test_utils.py
@@ -9,10 +9,12 @@ import requests
 def no_ssl_verification():
     """totally disable SSL verification, useful for Betamax tests"""
     old_request = requests.Session.request
-    requests.Session.request = partialmethod(old_request, verify=False)
+    try:
+        requests.Session.request = partialmethod(old_request, verify=False)
 
-    warnings.filterwarnings('ignore', 'Unverified HTTPS request')
-    yield
-    warnings.resetwarnings()
+        warnings.filterwarnings('ignore', 'Unverified HTTPS request')
+        yield
+    finally:
+        warnings.resetwarnings()
 
-    requests.Session.request = old_request
+        requests.Session.request = old_request
