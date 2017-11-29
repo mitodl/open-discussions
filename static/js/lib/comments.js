25e1500f0afd7a812b25ed884374bac2bb448f0b
George Schneeloch <gschneel@mit.edu>
2017-11-29 10:03:53 -0500

    Use continue

--- static/js/lib/comments.js
+++ static/js/lib/comments.js
@@ -14,27 +14,29 @@ export const findComment = (
   parentIndexes: Array<number> = []
 ): Object | null => {
   for (const [index, comment] of enumerate(commentTree)) {
-    if (comment.comment_type === "comment") {
-      if (comment.id === commentId) {
-        const path = []
-        for (const parentIndex of parentIndexes) {
-          path.push(parentIndex)
-          path.push("replies")
-        }
-        path.push(index)
-
-        return R.lensPath(path)
-      }
-
-      // parents is shared between calls but this won't cause a problem because
-      // once we find the comment we can stop searching and construct the return list
-      parentIndexes.push(index)
-      const innerChain = findComment(comment.replies, commentId, parentIndexes)
-      if (innerChain !== null) {
-        return innerChain
-      }
-      parentIndexes.pop()
+    if (comment.comment_type !== "comment") {
+      continue
     }
+
+    if (comment.id === commentId) {
+      const path = []
+      for (const parentIndex of parentIndexes) {
+        path.push(parentIndex)
+        path.push("replies")
+      }
+      path.push(index)
+
+      return R.lensPath(path)
+    }
+
+    // parents is shared between calls but this won't cause a problem because
+    // once we find the comment we can stop searching and construct the return list
+    parentIndexes.push(index)
+    const innerChain = findComment(comment.replies, commentId, parentIndexes)
+    if (innerChain !== null) {
+      return innerChain
+    }
+    parentIndexes.pop()
   }
 
   return null
