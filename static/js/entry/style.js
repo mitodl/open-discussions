1a9c698b3d835bce74247ef151c48e8bd6fa0a98
George Schneeloch <gschneel@mit.edu>
2017-12-01 14:48:46 -0500

    Remove react-mdl stuff

--- static/js/entry/style.js
+++ static/js/entry/style.js
@@ -1,5 +1,4 @@
 /* global SETTINGS:false */
 __webpack_public_path__ = SETTINGS.public_path // eslint-disable-line no-undef, camelcase
 
-import "../../scss/react-mdl-spinner.scss"
 import "../../scss/layout.scss"
