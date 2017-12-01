2eec65692d5fefa6f89a6dfbd64b75bee7af4f69
George Schneeloch <gschneel@mit.edu>
2017-12-01 14:38:40 -0500

    Switch to spinkit

--- static/js/components/SpinnerButton.js
+++ static/js/components/SpinnerButton.js
@@ -1,5 +1,4 @@
 import React from "react"
-import Spinner from "react-mdl/lib/Spinner"
 
 import type { ChildrenArray } from "react"
 
@@ -59,7 +58,20 @@ export default class SpinnerButton extends React.Component {
         className = ""
       }
       className = `${className} disabled-with-spinner`
-      children = <Spinner singleColor />
+      children = <div className="sk-fading-circle">
+        <div className="sk-circle1 sk-circle" />
+        <div className="sk-circle2 sk-circle" />
+        <div className="sk-circle3 sk-circle" />
+        <div className="sk-circle4 sk-circle" />
+        <div className="sk-circle5 sk-circle" />
+        <div className="sk-circle6 sk-circle" />
+        <div className="sk-circle7 sk-circle" />
+        <div className="sk-circle8 sk-circle" />
+        <div className="sk-circle9 sk-circle" />
+        <div className="sk-circle10 sk-circle" />
+        <div className="sk-circle11 sk-circle" />
+        <div className="sk-circle12 sk-circle" />
+      </div>
     }
 
     return (
