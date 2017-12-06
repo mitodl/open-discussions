c929700a2e2ffc9df78dea6c6814895da7fc87d6
Nathan Levesque <nlevesq@mit.edu>
2017-12-06 11:40:29 -0500

    Refactored CommentTree to make it classy

--- static/js/components/CommentTree.js
+++ static/js/components/CommentTree.js
@@ -29,180 +29,6 @@ import type { CommentVoteFunc } from "./CommentVoteForm"
 type LoadMoreCommentsFunc = (comment: MoreCommentsInTree) => Promise<*>
 type BeginEditingFunc = (fk: string, iv: Object, e: ?Object) => void
 
-const renderTopLevelComment = R.curry(
-  (
-    forms: FormsState,
-    upvote: CommentVoteFunc,
-    downvote: CommentVoteFunc,
-    loadMoreComments: LoadMoreCommentsFunc,
-    beginEditing: BeginEditingFunc,
-    processing: boolean,
-    comment: GenericComment,
-    idx: number
-  ) =>
-    <Card key={idx}>
-      <div className="top-level-comment">
-        {renderGenericComment(
-          forms,
-          upvote,
-          downvote,
-          loadMoreComments,
-          beginEditing,
-          processing,
-          0,
-          comment
-        )}
-      </div>
-    </Card>
-)
-
-const renderGenericComment = R.curry(
-  (
-    forms: FormsState,
-    upvote: CommentVoteFunc,
-    downvote: CommentVoteFunc,
-    loadMoreComments: LoadMoreCommentsFunc,
-    beginEditing: BeginEditingFunc,
-    processing: boolean,
-    depth: number,
-    comment: GenericComment
-  ) => {
-    if (comment.comment_type === "comment") {
-      return renderComment(
-        forms,
-        upvote,
-        downvote,
-        loadMoreComments,
-        beginEditing,
-        processing,
-        depth,
-        comment
-      )
-    } else if (comment.comment_type === "more_comments") {
-      return renderMoreComments(loadMoreComments, comment)
-    } else {
-      throw new Error("Unexpected comment_type")
-    }
-  }
-)
-
-const renderComment = (
-  forms: FormsState,
-  upvote: CommentVoteFunc,
-  downvote: CommentVoteFunc,
-  loadMoreComments: LoadMoreCommentsFunc,
-  beginEditing: BeginEditingFunc,
-  processing: boolean,
-  depth: number,
-  comment: CommentInTree
-) => {
-  const formKey = replyToCommentKey(comment)
-  const editFormKey = editCommentKey(comment)
-  const initialValue = getCommentReplyInitialValue(comment)
-
-  const atMaxDepth = depth + 1 >= SETTINGS.max_comment_depth
-  return (
-    <div className="comment" key={`comment-${comment.id}`}>
-      <img className="profile-image" src={comment.profile_image} />
-      <div className="comment-contents">
-        <div className="author-info">
-          <span className="author-name">
-            {comment.author_name}
-          </span>
-          <span>
-            {moment(comment.created).fromNow()}
-          </span>
-        </div>
-        <div className="row">
-          {R.has(editFormKey, forms)
-            ? <EditCommentForm
-              forms={forms}
-              comment={comment}
-              processing={processing}
-              editing
-            />
-            : <ReactMarkdown
-              disallowedTypes={["Image"]}
-              source={addEditedMarker(comment)}
-              escapeHtml
-            />}
-        </div>
-        <div className="row comment-actions">
-          <CommentVoteForm
-            comment={comment}
-            upvote={upvote}
-            downvote={downvote}
-          />
-          {atMaxDepth
-            ? null
-            : <div
-              className="comment-action-button"
-              onClick={e => {
-                beginEditing(formKey, initialValue, e)
-              }}
-            >
-              <a href="#">Reply</a>
-            </div>}
-          {SETTINGS.username === comment.author_id
-            ? <div
-              className="comment-action-button"
-              onClick={e => {
-                beginEditing(editFormKey, comment, e)
-              }}
-            >
-              <a href="#">Edit</a>
-            </div>
-            : null}
-        </div>
-        {atMaxDepth
-          ? null
-          : <div>
-            <ReplyToCommentForm
-              forms={forms}
-              comment={comment}
-              processing={processing}
-            />
-          </div>}
-        {atMaxDepth
-          ? null
-          : <div className="replies">
-            {R.map(
-              renderGenericComment(
-                forms,
-                upvote,
-                downvote,
-                loadMoreComments,
-                beginEditing,
-                processing,
-                depth + 1
-              ),
-              comment.replies
-            )}
-          </div>}
-      </div>
-    </div>
-  )
-}
-
-const renderMoreComments = (
-  loadMoreComments: LoadMoreCommentsFunc,
-  comment: MoreCommentsInTree
-) => {
-  return (
-    <div
-      className="more-comments"
-      key={`more-comments-${comment.parent_id || "null"}`} // will be null if parent_id is null, indicating root level
-    >
-      <SpinnerButton
-        className="load-more-comments"
-        onClickPromise={() => loadMoreComments(comment)}
-      >
-        Load More Comments
-      </SpinnerButton>
-    </div>
-  )
-}
-
 type CommentTreeProps = {
   comments: Array<GenericComment>,
   forms: FormsState,
@@ -213,27 +39,135 @@ type CommentTreeProps = {
   processing: boolean
 }
 
-const CommentTree = ({
-  comments,
-  forms,
-  upvote,
-  downvote,
-  loadMoreComments,
-  beginEditing,
-  processing
-  }: CommentTreeProps) =>
-  <div className="comments">
-    {R.addIndex(R.map)(
-      renderTopLevelComment(
-        forms,
-        upvote,
-        downvote,
-        loadMoreComments,
-        beginEditing,
-        processing
-      ),
-      comments
-    )}
-  </div>
+export default class CommentTree extends React.Component<*, *> {
+  props: CommentTreeProps
 
-export default CommentTree
+  renderComment = (depth: number, comment: CommentInTree) => {
+    const { forms, upvote, downvote, beginEditing, processing } = this.props
+    const formKey = replyToCommentKey(comment)
+    const editFormKey = editCommentKey(comment)
+    const initialValue = getCommentReplyInitialValue(comment)
+    // ramda can't determine arity here so use curryN
+    const renderGenericComment = R.curryN(2, this.renderGenericComment)(
+      depth + 1
+    )
+
+    const atMaxDepth = depth + 1 >= SETTINGS.max_comment_depth
+    return (
+      <div className="comment" key={`comment-${comment.id}`}>
+        <img className="profile-image" src={comment.profile_image} />
+        <div className="comment-contents">
+          <div className="author-info">
+            <span className="author-name">
+              {comment.author_name}
+            </span>
+            <span>
+              {moment(comment.created).fromNow()}
+            </span>
+          </div>
+          <div className="row">
+            {R.has(editFormKey, forms)
+              ? <EditCommentForm
+                forms={forms}
+                comment={comment}
+                processing={processing}
+                editing
+              />
+              : <ReactMarkdown
+                disallowedTypes={["Image"]}
+                source={addEditedMarker(comment)}
+                escapeHtml
+              />}
+          </div>
+          <div className="row comment-actions">
+            <CommentVoteForm
+              comment={comment}
+              upvote={upvote}
+              downvote={downvote}
+            />
+            {atMaxDepth
+              ? null
+              : <div
+                className="comment-action-button"
+                onClick={e => {
+                  beginEditing(formKey, initialValue, e)
+                }}
+              >
+                <a href="#">Reply</a>
+              </div>}
+            {SETTINGS.username === comment.author_id
+              ? <div
+                className="comment-action-button"
+                onClick={e => {
+                  beginEditing(editFormKey, comment, e)
+                }}
+              >
+                <a href="#">Edit</a>
+              </div>
+              : null}
+          </div>
+          {atMaxDepth
+            ? null
+            : <div>
+              <ReplyToCommentForm
+                forms={forms}
+                comment={comment}
+                processing={processing}
+              />
+            </div>}
+          {atMaxDepth
+            ? null
+            : <div className="replies">
+              {R.map(renderGenericComment, comment.replies)}
+            </div>}
+        </div>
+      </div>
+    )
+  }
+
+  renderMoreComments = (comment: MoreCommentsInTree) => {
+    const { loadMoreComments } = this.props
+    return (
+      <div
+        className="more-comments"
+        key={`more-comments-${comment.parent_id || "null"}`} // will be null if parent_id is null, indicating root level
+      >
+        <SpinnerButton
+          className="load-more-comments"
+          onClickPromise={() => loadMoreComments(comment)}
+        >
+          Load More Comments
+        </SpinnerButton>
+      </div>
+    )
+  }
+
+  renderGenericComment = (depth: number, comment: GenericComment) => {
+    if (comment.comment_type === "comment") {
+      return this.renderComment(depth, comment)
+    } else if (comment.comment_type === "more_comments") {
+      return this.renderMoreComments(comment)
+    } else {
+      throw new Error("Unexpected comment_type")
+    }
+  }
+
+  renderTopLevelComment = (comment: GenericComment, idx: number) => {
+    return (
+      <Card key={idx}>
+        <div className="top-level-comment">
+          {this.renderGenericComment(0, comment)}
+        </div>
+      </Card>
+    )
+  }
+
+  render() {
+    const { comments } = this.props
+    return (
+      <div className="comments">
+        {R.addIndex(R.map)(this.renderTopLevelComment, comments)}
+      </div>
+    )
+  }
+}
