91a1fd52d2c913f3ffb12c3939193e10076fbaa5
George Schneeloch <gschneel@mit.edu>
2017-12-29 17:50:48 -0500

    Have update-docker-hub update local dockerfiles

--- travis/update-docker-hub.sh
+++ travis/update-docker-hub.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 set -eo pipefail
 
-docker build -t mitodl/open_discussions_web_travis_next -f Dockerfile .
-docker build -t mitodl/open_discussions_watch_travis_next -f travis/Dockerfile-travis-watch-build .
+export NEXT=$(date | md5sum | cut -c -6)
+export PROJECT_NAME="open_discussions"
+echo "Next hash is $NEXT"
 
-docker push mitodl/open_discussions_web_travis_next
-docker push mitodl/open_discussions_watch_travis_next
+export WEB_IMAGE=mitodl/${PROJECT_NAME}_web_travis_${NEXT}
+export WATCH_IMAGE=mitodl/${PROJECT_NAME}_watch_travis_${NEXT}
+
+docker build -t $WEB_IMAGE -f Dockerfile .
+docker build -t $WATCH_IMAGE -f travis/Dockerfile-travis-watch-build .
+
+docker push $WEB_IMAGE
+docker push $WATCH_IMAGE
+
+sed -r -i "s|^FROM mitodl.+$|FROM $WEB_IMAGE|" travis/Dockerfile-travis-web
+sed -r -i "s|^FROM mitodl.+$|FROM $WATCH_IMAGE|" travis/Dockerfile-travis-watch
