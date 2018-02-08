2a2abdba82818c89c6e61e9d333dc143de93e9fd
Nathan Levesque <nlevesq@mit.edu>
2018-02-08 12:59:29 -0500

    Added Site models (#444)

---
+++ sites/admin.py
@@ -0,0 +1,12 @@
+"""Admin for sites"""
+from django.contrib import admin
+
+from sites.models import AuthenticatedSite
+
+
+class AuthenticatedSiteAdmin(admin.ModelAdmin):
+    """Admin for Authenticated site models"""
+    pass
+
+
+admin.site.register(AuthenticatedSite, AuthenticatedSiteAdmin)
---
+++ sites/apps.py
@@ -0,0 +1,7 @@
+"""Sites apps"""
+from django.apps import AppConfig
+
+
+class SitesConfig(AppConfig):
+    """Sites AppConfig"""
+    name = 'sites'
