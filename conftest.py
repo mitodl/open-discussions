08564ae717174fd62008b390871338931f257478
George Schneeloch <gschneel@mit.edu>
2018-03-14 10:44:31 -0400

    Upgrade Django to 1.11, other upgrades (#530)

--- conftest.py
+++ conftest.py
@@ -1,5 +1,7 @@
 """Configuration for pytest fixtures"""
 # pylint: disable=unused-argument, redefined-outer-name
+import warnings
+
 import factory
 import pytest
 from rest_framework.test import APIClient
@@ -9,6 +11,34 @@ from open_discussions.factories import UserFactory
 from sites.factories import AuthenticatedSiteFactory
 
 pytestmark = pytest.mark.usefixtures('authenticated_site')
+
+
+@pytest.fixture(autouse=True)
+def warnings_as_errors():
+    """
+    Convert warnings to errors. This should only affect unit tests, letting pylint and other plugins
+    raise DeprecationWarnings without erroring.
+    """
+    try:
+        warnings.resetwarnings()
+        warnings.simplefilter('error')
+        # For celery
+        warnings.simplefilter('ignore', category=ImportWarning)
+        warnings.filterwarnings(
+            "ignore",
+            message="'async' and 'await' will become reserved keywords in Python 3.7",
+            category=DeprecationWarning,
+        )
+        # Ignore deprecation warnings in third party libraries
+        warnings.filterwarnings(
+            "ignore",
+            module=".*(api_jwt|api_jws|rest_framework_jwt|betamax).*",
+            category=DeprecationWarning,
+        )
+
+        yield
+    finally:
+        warnings.resetwarnings()
 
 
 @pytest.fixture(scope="function")
--- open_discussions/templatetags/render_bundle.py
+++ open_discussions/templatetags/render_bundle.py
@@ -3,8 +3,8 @@
 from django import template
 from django.conf import settings
 from django.contrib.staticfiles.templatetags.staticfiles import static
+from django.utils.safestring import mark_safe
 
-from webpack_loader.templatetags.webpack_loader import render_as_tags
 from webpack_loader.utils import get_loader
 
 from open_discussions.utils import webpack_dev_server_url
@@ -31,6 +31,15 @@ def public_path(request):
 def _get_bundle(request, bundle_name):
     """
     Update bundle URLs to handle webpack hot reloading correctly if DEBUG=True
+
+    Args:
+        request (django.http.request.HttpRequest): A request
+        bundle_name (str): The name of the webpack bundle
+
+    Returns:
+        iterable of dict:
+            The chunks of the bundle. Usually there's only one but I suppose you could have
+            CSS and JS chunks for one bundle for example
     """
     if not settings.DISABLE_WEBPACK_LOADER_STATS:
         for chunk in get_loader('DEFAULT').get_bundle(bundle_name):
@@ -46,9 +55,46 @@ def _get_bundle(request, bundle_name):
 def render_bundle(context, bundle_name):
     """
     Render the script tags for a Webpack bundle
+
+    We use this instead of webpack_loader.templatetags.webpack_loader.render_bundle because we want to substitute
+    a dynamic URL for webpack dev environments. Maybe in the future we should refactor to use publicPath
+    instead for this.
+
+    Args:
+        context (dict): The context for rendering the template (includes request)
+        bundle_name (str): The name of the bundle to render
+
+    Returns:
+        django.utils.safestring.SafeText: The tags for JS and CSS
     """
     try:
-        return render_as_tags(_get_bundle(context['request'], bundle_name), '')
+        bundle = _get_bundle(context['request'], bundle_name)
+        return _render_tags(bundle)
     except OSError:
         # webpack-stats.json doesn't exist
-        return ''
+        return mark_safe('')
+
+
+def _render_tags(bundle):
+    """
+    Outputs tags for template rendering.
+    Adapted from webpack_loader.utils.get_as_tags and webpack_loader.templatetags.webpack_loader.
+
+    Args:
+        bundle (iterable of dict): The information about a webpack bundle
+
+    Returns:
+        django.utils.safestring.SafeText: HTML for rendering bundles
+    """
+
+    tags = []
+    for chunk in bundle:
+        if chunk['name'].endswith(('.js', '.js.gz')):
+            tags.append((
+                '<script type="text/javascript" src="{}" ></script>'
+            ).format(chunk['url']))
+        elif chunk['name'].endswith(('.css', '.css.gz')):
+            tags.append((
+                '<link type="text/css" href="{}" rel="stylesheet" />'
+            ).format(chunk['url']))
+    return mark_safe('\n'.join(tags))
