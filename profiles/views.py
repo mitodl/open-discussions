39068b201c81345c0006466a9ae0d656482b9d0c
Matt Bertrand <mattbert@mit.edu>
2018-05-14 14:39:51 -0400

    Additions to Profile model and DRF API (#695)
    * Updated Profile with additional fields and added and DRF API to view/update a profile.

--- profiles/views.py
+++ profiles/views.py
@@ -2,11 +2,13 @@
 
 from django.contrib.auth import get_user_model
 
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
 from rest_framework.permissions import IsAuthenticated
 
 from open_discussions.permissions import JwtIsStaffPermission
-from profiles.serializers import UserSerializer
+from profiles.models import Profile
+from profiles.permissions import HasEditPermission
+from profiles.serializers import UserSerializer, ProfileSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -16,3 +18,12 @@ class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     queryset = get_user_model().objects.all()
     lookup_field = 'username'
+
+
+class ProfileViewSet(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, viewsets.GenericViewSet):
+    """View for profile"""
+    permission_classes = (IsAuthenticated, HasEditPermission)
+
+    serializer_class = ProfileSerializer
+    queryset = Profile.objects.all()
+    lookup_field = 'user__username'
