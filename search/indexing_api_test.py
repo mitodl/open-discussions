8fe67059c3100e37944d9861da89ec596764baa9
George Schneeloch <gschneel@mit.edu>
2018-05-30 15:14:38 -0400

    Fix tests

--- search/indexing_api_test.py
+++ search/indexing_api_test.py
@@ -40,6 +40,7 @@ def mocked_es(mocker, settings):
         conn=conn,
         index_name=index_name,
         default_alias=get_default_alias_name(),
+        reindex_alias=get_reindexing_alias_name(),
     )
 
 
@@ -50,12 +51,13 @@ def test_create_document(mocked_es):
     doc_id, data = ('doc_id', {'key1': 'value1'})
     create_document(doc_id, data)
     mocked_es.get_conn.assert_called_once_with(verify=True)
-    mocked_es.conn.create.assert_called_once_with(
-        index=mocked_es.default_alias,
-        doc_type=GLOBAL_DOC_TYPE,
-        body=data,
-        id=doc_id,
-    )
+    for alias in mocked_es.default_alias, mocked_es.reindex_alias:
+        mocked_es.conn.create.assert_any_call(
+            index=alias,
+            doc_type=GLOBAL_DOC_TYPE,
+            body=data,
+            id=doc_id,
+        )
 
 
 def test_partially_update_document(mocked_es):
@@ -65,12 +67,13 @@ def test_partially_update_document(mocked_es):
     doc_id, data = ('doc_id', {'key1': 'value1'})
     update_document_with_partial(doc_id, data)
     mocked_es.get_conn.assert_called_once_with(verify=True)
-    mocked_es.conn.update.assert_called_once_with(
-        index=mocked_es.default_alias,
-        doc_type=GLOBAL_DOC_TYPE,
-        body={'doc': data},
-        id=doc_id,
-    )
+    for alias in mocked_es.default_alias, mocked_es.reindex_alias:
+        mocked_es.conn.update.assert_any_call(
+            index=alias,
+            doc_type=GLOBAL_DOC_TYPE,
+            body={'doc': data},
+            id=doc_id,
+        )
 
 
 def test_increment_document_integer_field(mocked_es):
@@ -81,17 +84,19 @@ def test_increment_document_integer_field(mocked_es):
     doc_id, field_name, incr_amount = ('doc_id', 'some_field_name', 1)
     increment_document_integer_field(doc_id, field_name, incr_amount)
     mocked_es.get_conn.assert_called_once_with(verify=True)
-    mocked_es.conn.update.assert_called_once_with(
-        index=mocked_es.default_alias,
-        doc_type=GLOBAL_DOC_TYPE,
-        body={
-            "script": {
-                "source": "ctx._source.{} += {}".format(field_name, incr_amount),
-                "lang": "painless",
-            }
-        },
-        id=doc_id,
-    )
+
+    for alias in mocked_es.default_alias, mocked_es.reindex_alias:
+        mocked_es.conn.update.assert_any_call(
+            index=alias,
+            doc_type=GLOBAL_DOC_TYPE,
+            body={
+                "script": {
+                    "source": "ctx._source.{} += {}".format(field_name, incr_amount),
+                    "lang": "painless",
+                }
+            },
+            id=doc_id,
+        )
 
 
 @pytest.mark.parametrize("skip_mapping", [True, False])
