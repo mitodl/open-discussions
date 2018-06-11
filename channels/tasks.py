61809fbc9956993df8abe9e957c5e6ec44f13e72
George Schneeloch <gschneel@mit.edu>
2018-06-11 15:25:13 -0400

    Test logging error handling

--- channels/tasks.py
+++ channels/tasks.py
@@ -3,7 +3,20 @@ from channels import api
 from open_discussions.celery import app
 
 
+from celery.utils.log import get_task_logger
+log = get_task_logger(__name__)
+
+
 @app.task()
 def evict_expired_access_tokens():
     """Evicts expired access tokens"""
     api.evict_expired_access_tokens()
+
+
+@app.task
+def log_test():
+    """Test logging error messages"""
+    try:
+        1/0
+    except:
+        log.exception("Testing celery logging")
