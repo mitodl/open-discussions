6294aff2b633c387b5cd74ffb43e83d550f3dc1d
Peter Pinch <430126+pdpinch@users.noreply.github.com>
2018-10-29 09:10:27 -0400

    add profile admin

---
+++ profiles/admin.py
@@ -0,0 +1,35 @@
+"""
+Admin site bindings for profiles
+"""
+
+from django.contrib import admin
+
+from .models import Profile
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    """Admin for Profile"""
+    model = Profile
+    list_display = [
+        'user',
+        'name',
+        'email_optin',
+        'toc_optin',
+    ]
+    search_fields = [
+        'name',
+        'user__email',
+        'user__username',
+    ]
+    list_filter = [
+        'email_optin',
+        'toc_optin',
+        'user__is_active',
+    ]
+    raw_id_fields = ('user',)
+    readonly_fields = ('image',
+        'image_small',
+        'image_medium',
+    )
+
+admin.site.register(Profile, ProfileAdmin)
