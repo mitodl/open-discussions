7e2b86b6ba12b314d80f009deb49e02613ddc425
Matt Bertrand <mattbert@mit.edu>
2019-02-11 17:08:43 -0500

    Reorder imports

--- search/serializers.py
+++ search/serializers.py
@@ -7,9 +7,9 @@ from rest_framework import serializers
 from channels.constants import POST_TYPE, COMMENT_TYPE
 from channels.models import Comment, Post
 from course_catalog.models import Course
-from profiles.utils import image_uri
 from profiles.api import get_channels
 from profiles.models import Profile
+from profiles.utils import image_uri
 from search.api import gen_post_id, gen_comment_id, gen_profile_id, gen_course_id
 from search.constants import PROFILE_TYPE, COURSE_TYPE
 from open_discussions.utils import filter_dict_keys, filter_dict_with_renamed_keys
