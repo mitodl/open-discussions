9dbf5e60e6245755a4ad7bb1710b89e2e3254ff1
George Schneeloch <gschneel@mit.edu>
2019-02-14 14:15:44 -0500

    Temporarily skip flaky test

--- static/js/components/widgets/WidgetEditDialog_test.js
+++ static/js/components/widgets/WidgetEditDialog_test.js
@@ -13,6 +13,7 @@ import WidgetEditDialog, {
   WIDGET_TYPE_SELECT
 } from "./WidgetEditDialog"
 import * as validationFuncs from "../../lib/validation"
+import { wait } from "../../lib/util"
 
 import {
   makeFieldSpec,
@@ -197,11 +198,11 @@ describe("WidgetEditDialog", () => {
       [false, WIDGET_EDIT, true],
       [false, WIDGET_EDIT, false]
     ].forEach(([expected, dialogState, dialogOpen]) => {
-      it(`${shouldIf(
+      it.skip(`${shouldIf(
         expected
       )} shift focus from radio buttons when state=${dialogState} and dialog ${isIf(
         dialogOpen
-      )} open`, () => {
+      )} open`, async () => {
         dialogData.state = dialogState
 
         const div = document.createElement("div")
