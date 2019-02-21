mitodl/open-discussions

commit c608af5f86f9928a9228dab3648c960dac6db5a1

add validtion to post editing
this adds our existing post form validation to post editing on the post
detail page. basically this will check for empty article-type posts and
for text posts that are over the character limit (40000).

closes #1682
pr #1852

static/js/lib/validation.js
@@ -68,7 +68,13 @@ const validateIfPostType = (
   postType: PostFormType,
   validator: Function
 ) => (postForm: { value: PostForm }) => {
-  if (R.isEmpty(postForm) || postForm.value.postType !== postType) {
+  if (R.isEmpty(postForm)) {
+    return S.Nothing
+  }
+
+  const formPostType = postForm.value.postType || postForm.value.post_type
+
+  if (formPostType !== postType) {
     return S.Nothing
   } else {
     return validator(postForm)
@@ -91,12 +97,32 @@ export const postURLValidation = validateIfPostType(
   }
 )
 
+const isEmptyArticle = (article: Array<Object>): boolean => {
+  // unfortunately detecting an empty article is non-trivial :/
+
+  // if it's an empty array that's easy
+  if (R.equals(article, [])) {
+    return true
+  }
+
+  // sometimes the editor will leave one empty tag in it
+  if (article.length === 1) {
+    const [obj] = article
+    if (R.equals(obj.attributes, {}) || R.equals(obj.children, [])) {
+      return true
+    }
+  }
+  return false
+}
+
 export const postArticleValidation = validateIfPostType(
   LINK_TYPE_ARTICLE,
   (postForm: { value: PostForm }) => {
     const { value: post } = postForm
-    if (R.equals(post.article, [])) {
-      return S.Just(formLensSetter("article", "Article must not be empty"))
+    if (isEmptyArticle(post.article_content)) {
+      return S.Just(
+        formLensSetter("article_content", "Article must not be empty")
+      )
     } else {
       return S.Nothing
     }

static/js/lib/validation_test.js
@@ -167,21 +167,25 @@ describe("validation library", () => {
 
     it("should complain about an empty article post", () => {
       const post = {
-        value: { postType: LINK_TYPE_ARTICLE, title: "potato", article: [] }
+        value: {
+          postType:        LINK_TYPE_ARTICLE,
+          title:           "potato",
+          article_content: []
+        }
       }
       assert.deepEqual(validatePostCreateForm(post), {
         value: {
-          article: "Article must not be empty"
+          article_content: "Article must not be empty"
         }
       })
     })
 
-    it("should allow an non-empty article post", () => {
+    it("should allow a non-empty article post", () => {
       const post = {
         value: {
-          postType: LINK_TYPE_ARTICLE,
-          title:    "potato",
-          article:  [{ hey: "there" }]
+          postType:        LINK_TYPE_ARTICLE,
+          title:           "potato",
+          article_content: [{ hey: "there" }]
         }
       }
       assert.deepEqual(validatePostCreateForm(post), {})

static/js/components/CommentForms.js
@@ -10,6 +10,7 @@ import ProfileImage, { PROFILE_IMAGE_MICRO } from "../containers/ProfileImage"
 import ArticleEditor from "./ArticleEditor"
 import CoverImageInput from "./CoverImageInput"
 
+import { validationMessage } from "../lib/validation"
 import { actions } from "../actions"
 import { clearCommentError } from "../actions/comment"
 import { setPostData } from "../actions/post"
@@ -20,6 +21,7 @@ import {
   preventDefaultAndInvoke
 } from "../lib/util"
 import { LINK_TYPE_ARTICLE } from "../lib/channels"
+import { validatePostCreateForm } from "../lib/validation"
 
 import type {
   CommentForm,
@@ -46,7 +48,8 @@ type CommentComponentProps = {
   patchPost: (p: Object) => void,
   comment: CommentInTree,
   editing: boolean,
-  profile?: Profile
+  profile?: Profile,
+  setValidationErrors: (errors: Object) => void
 }
 
 type CommentFormState = {
@@ -83,9 +86,9 @@ const userOrAnonymousFunction = (
     ? preventDefaultAndInvoke(anonymousFunc)
     : userFunc
 
-const ArticleInput: "ArticleInput" = "ArticleInput"
-const WYSIWYGInput: "WYSIWYGInput" = "WYSIWYGInput"
-const TextInput: "TextInput" = "TextInput"
+export const ArticleInput: "ArticleInput" = "ArticleInput"
+export const WYSIWYGInput: "WYSIWYGInput" = "WYSIWYGInput"
+export const TextInput: "TextInput" = "TextInput"
 type InputFormHelperInputType =
   | typeof ArticleInput
   | typeof WYSIWYGInput
@@ -102,10 +105,11 @@ type InputFormProps = {
   onTogglePopup?: Function,
   profile?: Profile,
   article?: Array<Object>,
-  inputType: InputFormHelperInputType
+  inputType: InputFormHelperInputType,
+  validation?: Object
 }
 
-const InputFormHelper = ({
+export const InputFormHelper = ({
   onSubmit,
   text,
   onUpdate,
@@ -116,25 +120,32 @@ const InputFormHelper = ({
   onTogglePopup,
   profile,
   article,
-  inputType
+  inputType,
+  validation
 }: InputFormProps) => {
   let input
   switch (inputType) {
   case ArticleInput:
     input = (
-      <ArticleEditor
-        initialData={article}
-        onChange={editorUpdateFormShim("article_content", onUpdate)}
-      />
+      <React.Fragment>
+        <ArticleEditor
+          initialData={article}
+          onChange={editorUpdateFormShim("article_content", onUpdate)}
+        />
+        {validation ? validationMessage(validation.article_content) : null}
+      </React.Fragment>
     )
     break
   case WYSIWYGInput:
     input = (
-      <Editor
-        initialValue={text || ""}
-        onChange={editorUpdateFormShim("text", onUpdate)}
-        autoFocus={autoFocus}
-      />
+      <React.Fragment>
+        <Editor
+          initialValue={text || ""}
+          onChange={editorUpdateFormShim("text", onUpdate)}
+          autoFocus={autoFocus}
+        />
+        {validation ? validationMessage(validation.text) : null}
+      </React.Fragment>
     )
     break
   case TextInput:
@@ -295,6 +306,13 @@ const mapDispatchToProps = (dispatch: any, ownProps: CommentComponentProps) => {
         }
       }
     }),
+    setValidationErrors: errors =>
+      dispatch(
+        actions.forms.formValidate({
+          formKey,
+          errors
+        })
+      ),
     patchComment: async comment => {
       try {
         await dispatch(actions.comments.patch(comment.id, comment))
@@ -445,26 +463,35 @@ export const EditPostForm: Class<React$Component<*, *>> = connect(
     }
 
     onSubmit = async e => {
-      const { formKey, forms, patchPost, post } = this.props
-      // eslint-disable-next-line camelcase
-      const { text, article_content, cover_image } = R.prop(
+      const {
         formKey,
-        forms
-      ).value
-
+        forms,
+        patchPost,
+        post,
+        setValidationErrors
+      } = this.props
       e.preventDefault()
-
-      const { id } = post
-      this.setState({ patching: true })
+      const form = R.prop(formKey, forms)
       // eslint-disable-next-line camelcase
-      const content =
-        post.post_type === LINK_TYPE_ARTICLE
-          ? { id, article_content, cover_image }
-          : { id, text }
-      try {
-        await patchPost(content)
-      } catch (_) {
-        this.setState({ patching: false })
+      const { text, article_content, cover_image } = form.value
+
+      const validation = validatePostCreateForm(form)
+
+      if (!R.isEmpty(validation)) {
+        setValidationErrors(validation)
+      } else {
+        const { id } = post
+        this.setState({ patching: true })
+        // eslint-disable-next-line camelcase
+        const content =
+          post.post_type === LINK_TYPE_ARTICLE
+            ? { id, article_content, cover_image }
+            : { id, text }
+        try {
+          await patchPost(content)
+        } catch (_) {
+          this.setState({ patching: false })
+        }
       }
     }
 
@@ -482,6 +509,8 @@ export const EditPostForm: Class<React$Component<*, *>> = connect(
         [formKey, "value", "article_content"],
         forms
       )
+
+      const validation = R.pathOr({}, [formKey, "errors", "value"], forms)
 
       const inputType =
         post.post_type === LINK_TYPE_ARTICLE ? ArticleInput : WYSIWYGInput
@@ -505,6 +534,7 @@ export const EditPostForm: Class<React$Component<*, *>> = connect(
             disabled={patching}
             autoFocus={true}
             inputType={inputType}
+            validation={validation}
           />
         </React.Fragment>
       )

static/js/components/CommentForms_test.js
@@ -3,7 +3,7 @@ import React from "react"
 import R from "ramda"
 import { Provider } from "react-redux"
 import { assert } from "chai"
-import { mount } from "enzyme"
+import { mount, shallow } from "enzyme"
 import sinon from "sinon"
 import { EditorView } from "prosemirror-view"
 
@@ -12,15 +12,20 @@ import {
   ReplyToPostForm,
   EditCommentForm,
   EditPostForm,
+  InputFormHelper,
   replyToPostKey,
   replyToCommentKey,
   editCommentKey,
   beginEditing,
   getCommentReplyInitialValue,
-  editPostKey
+  editPostKey,
+  ArticleInput,
+  WYSIWYGInput,
+  TextInput
 } from "./CommentForms"
 import Router from "../Router"
 import LoginPopup from "./LoginPopup"
+import Editor from "./Editor"
 
 import * as forms from "../actions/forms"
 import { actions } from "../actions"
@@ -607,20 +612,79 @@ describe("CommentForms", () => {
     })
   })
 
+  describe("InputFormhelper", () => {
+    let props
+
+    const renderInputFormHelper = (extraProps = {}) =>
+      shallow(<InputFormHelper {...props} {...extraProps} />)
+
+    beforeEach(() => {
+      props = {
+        onSubmit:      helper.sandbox.stub(),
+        text:          "",
+        onUpdate:      helper.sandbox.stub(),
+        cancelReply:   helper.sandbox.stub(),
+        isComment:     false,
+        disabled:      false,
+        autoFocus:     false,
+        onTogglePopup: helper.sandbox.stub(),
+        profile:       null,
+        article:       null
+      }
+    })
+
+    it("should use an <Editor /> if WYSIWYGInput", () => {
+      const wrapper = renderInputFormHelper({
+        inputType: WYSIWYGInput
+      })
+      assert.ok(wrapper.find(Editor).exists())
+    })
+
+    it("should use a text input if TextInput", () => {
+      const wrapper = renderInputFormHelper({
+        inputType: TextInput
+      })
+      assert.ok(wrapper.find("textarea.input").exists())
+    })
+
+    it("should use an article editor if ArticleInput", () => {
+      const wrapper = renderInputFormHelper({
+        inputType: ArticleInput
+      })
+      assert.ok(wrapper.find("ArticleEditor").exists())
+    })
+
+    it("should show article validation errors", () => {
+      const wrapper = renderInputFormHelper({
+        inputType:  ArticleInput,
+        validation: {
+          article_content: "BAD"
+        }
+      })
+      assert.equal(wrapper.find(".validation-message").text(), "BAD")
+    })
+
+    it("should show text validation errors", () => {
+      const wrapper = renderInputFormHelper({
+        inputType:  WYSIWYGInput,
+        validation: {
+          text: "BAD"
+        }
+      })
+      assert.equal(wrapper.find(".validation-message").text(), "BAD")
+    })
+  })
+
   describe("EditPostForm", () => {
     let wrapper
 
     beforeEach(() => {
       wrapper = renderEditPostForm()
-    })
-
-    it("should use the <Editor /> if a text post", () => {
-      assert.ok(wrapper.find("Editor").exists())
-      assert.isNotOk(wrapper.find("textarea").exists())
+      helper.getCKEditorJWTStub.returns(Promise.resolve())
     })
 
     it("should use the ArticleEditor and CoverImageInput if an article post", () => {
-      post.article = []
+      post.article_content = []
       wrapper = renderEditPostForm()
       assert.ok(wrapper.find("ArticleEditor"))
       assert.ok(wrapper.find("CoverImageInput"))

static/js/components/CreatePostForm.js
@@ -158,9 +158,11 @@ export default class CreatePostForm extends React.Component<Props, State> {
             />
           ) : null}
           {validationMessage(validation.coverImage)}
-          <ArticleEditor onChange={editorUpdateFormShim("article", onUpdate)} />
+          <ArticleEditor
+            onChange={editorUpdateFormShim("article_content", onUpdate)}
+          />
           {this.clearInputButton()}
-          {validationMessage(validation.article)}
+          {validationMessage(validation.article_content)}
         </div>
       )
     } else {

static/js/components/CreatePostForm_test.js
@@ -108,7 +108,7 @@ describe("CreatePostForm", () => {
   it("should show article validation message", () => {
     const postForm = { ...newPostForm(), postType: LINK_TYPE_ARTICLE }
     const wrapper = renderPostForm({
-      validation: { article: "WHAT?! NO! OF COURSE NOT!" },
+      validation: { article_content: "WHAT?! NO! OF COURSE NOT!" },
       postForm
     })
     assert.equal(

static/js/containers/CreatePostPage.js
@@ -67,7 +67,7 @@ const getForm = R.prop(CREATE_POST_KEY)
 
 const createPostPayload = (postForm: PostForm): CreatePostPayload => {
   // eslint-disable-next-line camelcase
-  const { postType, title, url, text, article, cover_image } = postForm
+  const { postType, title, url, text, article_content, cover_image } = postForm
 
   switch (postType) {
   case LINK_TYPE_LINK:
@@ -75,7 +75,7 @@ const createPostPayload = (postForm: PostForm): CreatePostPayload => {
   case LINK_TYPE_TEXT:
     return { title, text }
   case LINK_TYPE_ARTICLE:
-    return { title, article, cover_image }
+    return { title, article_content, cover_image }
   }
 
   // if no postType was selected, we're dealing with a 'title only' post
@@ -121,7 +121,7 @@ const shouldFormReset = (
     allEmptyOrNil([
       postForm.value.url,
       postForm.value.text,
-      postForm.value.article
+      postForm.value.article_content
     ])
   ) {
     return true
@@ -194,7 +194,7 @@ class CreatePostPage extends React.Component<CreatePostPageProps> {
         url:              "",
         text:             "",
         thumbnail:        null,
-        article:          [],
+        article_content:  [],
         show_cover_image: true
       })
     }
@@ -238,7 +238,7 @@ class CreatePostPage extends React.Component<CreatePostPageProps> {
       postType,
       url:              "",
       text:             "",
-      article:          [],
+      article_content:  [],
       cover_image:      null,
       show_cover_image: true
     })

static/js/containers/CreatePostPage_test.js
@@ -584,7 +584,7 @@ describe("CreatePostPage", () => {
                   : toChannelTypes[0],
               url:              "",
               text:             "",
-              article:          [],
+              article_content:  [],
               thumbnail:        null,
               show_cover_image: true
             })
@@ -656,7 +656,8 @@ describe("CreatePostPage", () => {
         const url =
           hasInput && postType === LINK_TYPE_LINK ? "http://foo.edu" : ""
         const text = hasInput && postType === LINK_TYPE_TEXT ? "test text" : ""
-        const article =
+        // eslint-disable-next-line camelcase
+        const article_content =
           hasInput && postType === LINK_TYPE_ARTICLE ? [{ foo: "bar" }] : null
         const props: any = {
           dispatch,
@@ -665,7 +666,7 @@ describe("CreatePostPage", () => {
               postType: postType,
               url,
               text,
-              article
+              article_content
             }
           },
           channel: currentChannel
@@ -685,7 +686,7 @@ describe("CreatePostPage", () => {
             url:              "",
             text:             "",
             thumbnail:        null,
-            article:          [],
+            article_content:  [],
             show_cover_image: true
           })
         } else {

commit 6adfe4a7a4d00d933dc40165479fdaf48a8646e0

fix an issue with article validation
I made a typo in the article validation, specifically in the function
that I wrote to check for an empty article (I `&&`-ed where I should
have `||`-ed :/ )

bug introduced in #1852
pr #1873

static/js/lib/validation.js
@@ -108,7 +108,7 @@ const isEmptyArticle = (article: Array<Object>): boolean => {
   // sometimes the editor will leave one empty tag in it
   if (article.length === 1) {
     const [obj] = article
-    if (R.equals(obj.attributes, {}) || R.equals(obj.children, [])) {
+    if (R.equals(obj.attributes, {}) && R.equals(obj.children, [])) {
       return true
     }
   }

static/js/lib/validation_test.js
@@ -165,18 +165,32 @@ describe("validation library", () => {
       })
     })
 
-    it("should complain about an empty article post", () => {
-      const post = {
-        value: {
-          postType:        LINK_TYPE_ARTICLE,
-          title:           "potato",
-          article_content: []
+    //
+    ;[
+      [[], true],
+      [[{ attributes: {}, children: [] }], true],
+      [[{ attributes: {}, children: [{ hey: "there" }] }], false]
+    ].forEach(([articleContent, shouldGiveError]) => {
+      it(`${shouldIf(shouldGiveError)} if articleContent = ${JSON.stringify(
+        articleContent
+      )}`, () => {
+        const post = {
+          value: {
+            postType:        LINK_TYPE_ARTICLE,
+            title:           "potato",
+            article_content: articleContent
+          }
         }
-      }
-      assert.deepEqual(validatePostCreateForm(post), {
-        value: {
-          article_content: "Article must not be empty"
-        }
+        assert.deepEqual(
+          validatePostCreateForm(post),
+          shouldGiveError
+            ? {
+              value: {
+                article_content: "Article must not be empty"
+              }
+            }
+            : {}
+        )
       })
     })
 