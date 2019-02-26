8c787c3aa1bac87a30ffd8655f7ad8297eab3b80
Alice Pote <alice.writes.wrongs@gmail.com>
2019-02-26 13:50:15 -0500

    get rid of the docker setup for JavaScript tests on travis
    Before we had a system of building docker images, pushing them to docker
    hub, and using those to cache dependencies and some environment setup
    between our travis runs. This wasn't a great system though overall, and
    wasn't really necessary.
    
    This commit changes things so that we instead dispense with the docker
    containers and just run the JS tests directly in travis' JS environment.
    This lets us take advantage of the dependency cacing and so on that
    travis already has between runs, and avoids the overhead of pulling down
    the docker image.
    
    pr #1884

--- travis/update-docker-hub.sh
+++ travis/update-docker-hub.sh
@@ -6,13 +6,9 @@ export PROJECT_NAME="open_discussions"
 echo "Next hash is $NEXT"
 
 export WEB_IMAGE=mitodl/${PROJECT_NAME}_web_travis_${NEXT}
-export WATCH_IMAGE=mitodl/${PROJECT_NAME}_watch_travis_${NEXT}
 
 docker build -t $WEB_IMAGE -f Dockerfile .
-docker build -t $WATCH_IMAGE -f travis/Dockerfile-travis-watch-build .
 
 docker push $WEB_IMAGE
-docker push $WATCH_IMAGE
 
 sed -r -i "s|^FROM mitodl.+$|FROM $WEB_IMAGE|" travis/Dockerfile-travis-web
-sed -r -i "s|^FROM mitodl.+$|FROM $WATCH_IMAGE|" travis/Dockerfile-travis-watch
