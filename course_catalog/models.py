52e60f7310297240054dac18dce178e2f489b37e
kfantastic <kyangfan@gmail.com>
2019-06-19 13:21:46 -0400

    adds unique_together constraint on FavoriteItem

--- course_catalog/models.py
+++ course_catalog/models.py
@@ -203,3 +203,6 @@ class FavoriteItem(TimestampedModel):
     )
     object_id = models.PositiveIntegerField()
     item = GenericForeignKey("content_type", "object_id")
+
+    class Meta:
+        unique_together = ("user", "content_type", "object_id")
