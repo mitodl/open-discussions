f4875a16aecf3550495bc78ae03188892d443527
Sean Esterkin <sean@zagaran.com>
2019-06-19 13:11:10 -0400

    Add test for generic foreign key errors

--- course_catalog/serializers_test.py
+++ course_catalog/serializers_test.py
@@ -9,8 +9,9 @@ from course_catalog.factories import (
     CoursePriceFactory,
     CourseInstructorFactory,
     BootcampFactory,
-)
-from course_catalog.serializers import CourseSerializer, BootcampSerializer
+    ProgramFactory)
+from course_catalog.models import ProgramItem
+from course_catalog.serializers import CourseSerializer, BootcampSerializer, ProgramItemSerializer
 
 pytestmark = pytest.mark.django_db
 
@@ -53,3 +54,18 @@ def test_serialize_bootcamp_related_models():
         assert attr in serializer.data["instructors"][0].keys()
     assert len(serializer.data["topics"]) == 3
     assert "name" in serializer.data["topics"][0].keys()
+
+
+def test_generic_foreign_key_serializer():
+    """
+    Test that generic foreign key serializer properly rejects unexpected classes
+    """
+    program = ProgramFactory()
+    course_topic = CourseTopicFactory()
+    program_item = ProgramItem(
+        program=program,
+        item=course_topic,
+    )
+    serializer = ProgramItemSerializer(program_item)
+    with pytest.raises(Exception):
+        assert serializer.data.get('item').get('id') == course_topic.id
