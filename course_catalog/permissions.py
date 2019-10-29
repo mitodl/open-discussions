4e256a691c07d98a1cae178902cd6f68ff2292d5
Matt Bertrand <mattbert@mit.edu>
2019-10-29 12:44:39 -0400

    remove debugging logging

--- course_catalog/permissions.py
+++ course_catalog/permissions.py
@@ -6,10 +6,6 @@ from rest_framework.permissions import SAFE_METHODS, BasePermission
 
 from course_catalog.constants import PrivacyLevel
 
-import logging
-
-log = logging.getLogger()
-
 
 class HasUserListPermissions(BasePermission):
     """Permission to view/modify UserLists"""
@@ -20,7 +16,6 @@ class HasUserListPermissions(BasePermission):
         return not request.user.is_anonymous
 
     def has_object_permission(self, request, view, obj):
-        log.error(view.action)
         if request.method in SAFE_METHODS or view.action in ("favorite", "unfavorite"):
             return (
                 obj.privacy_level == PrivacyLevel.public.value
