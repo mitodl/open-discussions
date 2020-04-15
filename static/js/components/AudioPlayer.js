1f6c448f82ca25190f043f3b61b1d1b7f1e45621
Carey Gumaer <gumaer.carey@gmail.com>
2020-04-15 17:14:23 -0400

    convert to hooks workflow

--- static/js/components/AudioPlayer.js
+++ static/js/components/AudioPlayer.js
@@ -1,6 +1,6 @@
 // @flow
 /* global SETTINGS: false */
-import React from "react"
+import React, { useState, useEffect, useRef } from "react"
 import Amplitude from "amplitudejs"
 
 type Props = {
@@ -9,45 +9,42 @@ type Props = {
   url: string
 }
 
-export default class AudioPlayer extends React.Component<Props> {
-  seekBar: {
-    current: null | React$ElementRef<typeof HTMLElement>
-  }
+export default function AudioPlayer(props: Props) {
+  const seekBar = useRef()
+  const [title, setTitle] = useState(props.title)
+  const [description, setDescription] = useState(props.description)
+  const [url, setUrl] = useState(props.url)
 
-  constructor(props: Props) {
-    super(props)
-    this.seekBar = React.createRef()
-    this.seekBarClick = this.seekBarClick.bind(this)
-    this.backTenClick = this.backTenClick.bind(this)
-    this.forwardThirtyClick = this.forwardThirtyClick.bind(this)
-  }
-
-  componentDidMount() {
-    Amplitude.init({
-      songs: [
-        {
-          album: this.props.title,
-          name:  this.props.description,
-          url:   this.props.url
-        }
-      ],
-      waveforms: {
-        sample_rate: 3000
+  useEffect(
+    () => {
+      if (!title || !description || !url) {
+        return
       }
-    })
-  }
+      Amplitude.init({
+        songs: [
+          {
+            album: title,
+            name:  description,
+            url:   url
+          }
+        ],
+        waveforms: {
+          sample_rate: 3000
+        }
+      })
+    },
+    [title, description, url]
+  )
 
-  componentWillUnmount() {}
-
-  backTenClick = () => {
+  const backTenClick = () => {
     Amplitude.skipTo(Amplitude.getSongPlayedSeconds() - 10, 0, null)
   }
 
-  forwardThirtyClick = () => {
+  const forwardThirtyClick = () => {
     Amplitude.skipTo(Amplitude.getSongPlayedSeconds() + 30, 0, null)
   }
 
-  seekBarClick = (e: Object) => {
+  const seekBarClick = (e: Object) => {
     const seekBar = this.seekBar && this.seekBar.current
     if (seekBar && seekBar.getBoundingClientRect && seekBar.offsetWidth) {
       const offset = seekBar.getBoundingClientRect()
@@ -59,58 +56,56 @@ export default class AudioPlayer extends React.Component<Props> {
     }
   }
 
-  render() {
-    return (
-      <div className="audio-player-container-outer">
-        <div className="audio-player-container-inner">
-          <div className="audio-player-titles">
-            <div
-              className="audio-player-podcast-name"
-              data-amplitude-song-info="album"
-            />
-            <div
-              className="audio-player-podcast-title"
-              data-amplitude-song-info="name"
-            />
+  return (
+    <div className="audio-player-container-outer">
+      <div className="audio-player-container-inner">
+        <div className="audio-player-titles">
+          <div
+            className="audio-player-podcast-name"
+            data-amplitude-song-info="album"
+          />
+          <div
+            className="audio-player-podcast-title"
+            data-amplitude-song-info="name"
+          />
+        </div>
+        <div className="audio-player-controls">
+          <div className="audio-player-button" onClick={backTenClick}>
+            <span className="material-icons">replay_10</span>
           </div>
-          <div className="audio-player-controls">
-            <div className="audio-player-button" onClick={this.backTenClick}>
-              <span className="material-icons">replay_10</span>
-            </div>
-            <div
-              className="audio-player-button amplitude-play-pause"
-              id="play-pause"
-            >
-              <span className="material-icons" />
-            </div>
-            <div
-              className="audio-player-button"
-              onClick={this.forwardThirtyClick}
-            >
-              <span className="material-icons">forward_30</span>
-            </div>
-            <div className="audio-player-progress-container">
-              <time className="audio-player-time-text-left">
-                <span className="amplitude-current-minutes" />:
-                <span className="amplitude-current-seconds" />
-              </time>
-              <progress
-                className="amplitude-song-played-progress audio-player-progress"
-                id="song-played-progress"
-                ref={this.seekBar}
-                onClick={this.seekBarClick}
-              />
-              <time className="audio-player-time-text-right">
-                <span className="amplitude-duration-minutes" />:
-                <span className="amplitude-duration-seconds" />
-              </time>
-            </div>
-            <div className="audio-player-playback-speed-container">
-              <button className="light-outlined amplitude-playback-speed" />
-            </div>
+          <div
+            className="audio-player-button amplitude-play-pause"
+            id="play-pause"
+          >
+            <span className="material-icons" />
+          </div>
+          <div
+            className="audio-player-button"
+            onClick={forwardThirtyClick}
+          >
+            <span className="material-icons">forward_30</span>
+          </div>
+          <div className="audio-player-progress-container">
+            <time className="audio-player-time-text-left">
+              <span className="amplitude-current-minutes" />:
+              <span className="amplitude-current-seconds" />
+            </time>
+            <progress
+              className="amplitude-song-played-progress audio-player-progress"
+              id="song-played-progress"
+              ref={seekBar}
+              onClick={seekBarClick}
+            />
+            <time className="audio-player-time-text-right">
+              <span className="amplitude-duration-minutes" />:
+              <span className="amplitude-duration-seconds" />
+            </time>
+          </div>
+          <div className="audio-player-playback-speed-container">
+            <button className="light-outlined amplitude-playback-speed" />
           </div>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
