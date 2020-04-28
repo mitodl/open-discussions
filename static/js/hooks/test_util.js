964adcb5793fa532d4b771087727447d81069eb4
Alice Pote <alice.writes.wrongs@gmail.com>
2020-04-28 11:10:51 -0400

    add test coverage for some utility hooks
    I just noticed that these were either not being tested or were being
    tested in a weird way, so I went ahead and added a few tests using some
    of the strategy I came up with for testing hooks when developing the
    podcast drawer stuff (#2854).
    
    pr #2867

--- static/js/hooks/test_util.js
+++ static/js/hooks/test_util.js
@@ -1,5 +1,5 @@
 // @flow
-import React from "react"
+import React, { useEffect, useRef } from "react"
 
 export const hookClickTestHarness = (hook: Function) => {
   function TestHarness(props: Object) {
@@ -16,6 +16,37 @@ export const genericTestHarness = (hook: Function) => {
   function TestHarness() {
     hook()
     return <div />
+  }
+  return TestHarness
+}
+
+export const hookReturnTestHarness = (hook: Function) => {
+  function TestHarness(props: Object) {
+    const { cb } = props
+    const result = hook()
+    cb(result)
+    return <div />
+  }
+  return TestHarness
+}
+
+export const renderCountTestHarness = (hook: Function) => {
+  function PropRecipient() {
+    // this is just a dummy component because React complains
+    // if you pass arbitrary props to e.g. a div or p tag
+    return <div />
+  }
+
+  function TestHarness() {
+    const ref = useRef(1)
+
+    useEffect(() => {
+      ref.current = ref.current + 1
+    })
+
+    hook()
+
+    return <PropRecipient renderCount={ref.current} />
   }
   return TestHarness
 }
--- static/js/hooks/util_test.js
+++ static/js/hooks/util_test.js
@@ -1,14 +1,17 @@
 // @flow
-import React from "react"
 import { assert } from "chai"
 
 import IntegrationTestHelper from "../util/integration_test_helper"
 
-import { useDeviceCategory } from "./util"
+import { useDeviceCategory, useResponsive } from "./util"
 import { PHONE, TABLET, DESKTOP } from "../lib/constants"
+import { hookReturnTestHarness, renderCountTestHarness } from "./test_util"
+
+const DeviceCategoryTestHarness = hookReturnTestHarness(useDeviceCategory)
+const UseResponsiveTestHarness = renderCountTestHarness(useResponsive)
 
 describe("utility hooks", () => {
-  let helper
+  let helper, render
 
   beforeEach(() => {
     helper = new IntegrationTestHelper()
@@ -19,16 +22,8 @@ describe("utility hooks", () => {
   })
 
   describe("useDeviceCategory", () => {
-    const TestComponent = () => {
-      const category = useDeviceCategory()
-
-      return <div>{category}</div>
-    }
-
-    let render
-
     beforeEach(() => {
-      render = helper.configureHOCRenderer(TestComponent, TestComponent, {})
+      render = helper.configureReduxQueryRenderer(DeviceCategoryTestHarness)
     })
 
     //
@@ -43,9 +38,24 @@ describe("utility hooks", () => {
     ].forEach(([width, expectation]) => {
       it(`should give ${expectation} for a width of ${width}`, async () => {
         helper.getViewportWidthStub.returns(width)
-        const { wrapper } = await render()
-        assert.equal(wrapper.text(), expectation)
+        await render({
+          cb: text => assert.equal(text, expectation)
+        })
       })
+    })
+  })
+
+  describe("useResponsive", () => {
+    beforeEach(() => {
+      render = helper.configureReduxQueryRenderer(UseResponsiveTestHarness)
+    })
+
+    it("should re-render after resize event", async () => {
+      const { wrapper } = await render()
+      assert.equal(wrapper.find("PropRecipient").prop("renderCount"), 1)
+      window.dispatchEvent(new Event("resize"))
+      wrapper.update()
+      assert.equal(wrapper.find("PropRecipient").prop("renderCount"), 2)
     })
   })
 })
