c421ceab44dc2d924c680138963e01a659c9ded0
Carey Gumaer <gumaer.carey@gmail.com>
2020-05-04 11:25:22 -0400

    fmt

--- static/js/components/material/Snackbar.js
+++ static/js/components/material/Snackbar.js
@@ -4,17 +4,12 @@ import { useSelector } from "react-redux"
 import { createSelector } from "reselect"
 import { Snackbar as RMWCSnackbar } from "@rmwc/snackbar"
 
-const getSnackbarState = createSelector(
-  state => state.ui,
-  ui => ui.snackbar
-)
+const getSnackbarState = createSelector(state => state.ui, ui => ui.snackbar)
 
 export default function Snackbar() {
   const snackbar = useSelector(getSnackbarState)
   console.log(snackbar)
   const message = snackbar ? snackbar.message : null
   const open = message ? true : false
-  return (
-    <RMWCSnackbar open={open} message={message} />
-  )
+  return <RMWCSnackbar open={open} message={message} />
 }
