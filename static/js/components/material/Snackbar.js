228d8ddd4de641e68acd23bb017cbc16c6269efa
Carey Gumaer <gumaer.carey@gmail.com>
2020-05-04 11:23:23 -0400

    update snackbar, change our implementation to use RMWC

--- static/js/components/material/Snackbar.js
+++ static/js/components/material/Snackbar.js
@@ -1,52 +1,20 @@
 // @flow
 import React from "react"
-import R from "ramda"
+import { useSelector } from "react-redux"
+import { createSelector } from "reselect"
+import { Snackbar as RMWCSnackbar } from "@rmwc/snackbar"
 
-import { MDCSnackbar } from "@material/snackbar/dist/mdc.snackbar"
+const getSnackbarState = createSelector(
+  state => state.ui,
+  ui => ui.snackbar
+)
 
-import type { SnackbarState } from "../../reducers/ui"
-
-type Props = {
-  snackbar: ?SnackbarState
-}
-
-export default class Snackbar extends React.Component<Props> {
-  snackbar = null
-  snackbarRoot = null
-
-  componentDidMount() {
-    this.snackbar = new MDCSnackbar(this.snackbarRoot)
-  }
-
-  // see here: https://reactjs.org/docs/react-component.html#unsafe_componentwillreceiveprops
-  // this method will be deprecated soon, so we need to refactor away from it somehow
-  // eslint-disable-next-line camelcase
-  UNSAFE_componentWillReceiveProps(nextProps: Props) {
-    if (!R.equals(this.props.snackbar, nextProps.snackbar)) {
-      this.showSnackbar(nextProps.snackbar)
-    }
-  }
-
-  showSnackbar(snackbar: ?SnackbarState) {
-    if (this.snackbar && snackbar) {
-      this.snackbar.show(R.omit("id", snackbar))
-    }
-  }
-
-  render() {
-    return (
-      <div
-        className="mdc-snackbar"
-        aria-live="assertive"
-        aria-atomic="true"
-        aria-hidden="true"
-        ref={node => (this.snackbarRoot = node)}
-      >
-        <div className="mdc-snackbar__text" />
-        <div className="mdc-snackbar__action-wrapper">
-          <button type="button" className="mdc-snackbar__action-button" />
-        </div>
-      </div>
-    )
-  }
+export default function Snackbar() {
+  const snackbar = useSelector(getSnackbarState)
+  console.log(snackbar)
+  const message = snackbar ? snackbar.message : null
+  const open = message ? true : false
+  return (
+    <RMWCSnackbar open={open} message={message} />
+  )
 }
