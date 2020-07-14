df901242ef904d5523cc02bed6d138872d242049
Matt Bertrand <mattbert@mit.edu>
2020-07-14 16:46:08 -0400

    help text

--- mail/models.py
+++ mail/models.py
@@ -1,7 +1,17 @@
 """ Models for mail app """
 from django.db import models
+from django.utils.safestring import mark_safe
 
 from open_discussions.models import TimestampedModel
+
+
+HELP_TEXT="""
+@spam.com: blocks all emails containing `@spam.com` like `joe@spam.com.edu`<br/>
+@spam.com$: blocks all emails ending in `@spam.com` like `joe@spam.com`<br/>
+spam.com: blocks all emails containing `spam.com` like `joe@antispam.com.edu`<br/>
+sue@gmail.com: blocks `sue@gmail.com` and `bobbysue@gmail.com`<br/>
+^sue@gmail.com: blocks `sue@gmail.com` but not `bobbysue@gmail.com`
+"""
 
 
 class BlockedEmailRegex(TimestampedModel):
@@ -9,4 +19,4 @@ class BlockedEmailRegex(TimestampedModel):
     An object indicating emails to block based on a matching regex string
     """
 
-    match = models.CharField(max_length=256, null=False, blank=False)
+    match = models.CharField(max_length=256, null=False, blank=False, help_text=mark_safe(HELP_TEXT))
