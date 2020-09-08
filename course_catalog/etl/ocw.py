a6b56634cd81f040c9ed1afae81bcb462b8166cb
abeglova <abeglova@mit.edu>
2020-09-08 16:52:41 -0400

    add level and department to search

--- course_catalog/etl/ocw.py
+++ course_catalog/etl/ocw.py
@@ -21,6 +21,46 @@ from course_catalog.models import ContentFile, get_max_length
 
 log = logging.getLogger()
 
+OCW_DEPARTMENT_CODE_TO_NAME = {
+    "1": "Civil and Environmental Engineering",
+    "2": "Mechanical Engineering",
+    "3": "Materials Science and Engineering",
+    "4": "Architecture",
+    "5": "Chemistry",
+    "6": "Electrical Engineering and Computer Science",
+    "7": "Biology",
+    "8": "Physics",
+    "9": "Brain and Cognitive Sciences",
+    "10": "Chemical Engineering",
+    "11": "Urban Studies and Planning",
+    "12": "Earth, Atmospheric, and Planetary Sciences",
+    "14": "Economics",
+    "15": "Sloan School of Management",
+    "16": "Aeronautics and Astronautics",
+    "17": "Political Science",
+    "18": "Mathematics",
+    "20": "Biological Engineering",
+    "21A": "Anthropology",
+    "21G": "Global Studies and Languages",
+    "21H": "History",
+    "21L": "Literature",
+    "21M": "Music and Theater Arts",
+    "22": "Nuclear Science and Engineering",
+    "24": "Linguistics and Philosophy",
+    "CC": "Concourse",
+    "CMS-W": "Comparative Media Studies/Writing",
+    "EC": "Edgerton Center",
+    "ES": "Experimental Study Group",
+    "ESD": "Engineering Systems Division",
+    "HST": "Health Sciences and Technology",
+    "IDS": "Institute for Data, Systems, and Society",
+    "MAS": "Media Arts and Sciences",
+    "PE": "Athletics, Physical Education and Recreation",
+    "RES": "Supplemental Resources",
+    "STS": "Science, Technology, and Society",
+    "WGS": "Women's and Gender Studies",
+}
+
 
 def get_ocw_learning_course_bucket():
     """
