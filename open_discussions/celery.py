942455019adca96393ebe70c32af43f6e8ba1256
abeglova <abeglova@mit.edu>
2020-11-02 15:07:00 -0500

    speparate celery queue for spam check tasks

--- open_discussions/celery.py
+++ open_discussions/celery.py
@@ -12,4 +12,12 @@ from django.conf import settings  # noqa pylint: disable=wrong-import-position
 
 app = Celery("open_discussions")
 app.config_from_object("django.conf:settings", namespace="CELERY")
+app.conf.task_default_queue = "default"
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)  # pragma: no cover
+
+app.conf.task_routes = {
+    "channels.tasks.check_post_for_spam": {"queue": "spam"},
+    "channels.tasks.check_comment_for_spam": {"queue": "spam"},
+    "channels.tasks.update_spam": {"queue": "spam"},
+    "channels.tasks.retire_user": {"queue": "spam"},
+}
