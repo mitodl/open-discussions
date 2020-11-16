32723cc7b2e9e811c483ca25fc76fa2fbdecce9f
Doof <mitx-devops@mit.edu>
2020-11-16 19:46:03 +0000

    Release 0.150.0

--- open_discussions/settings.py
+++ open_discussions/settings.py
@@ -29,7 +29,7 @@ from open_discussions.envs import (
 )
 from open_discussions.sentry import init_sentry
 
-VERSION = "0.149.2"
+VERSION = "0.150.0"
 
 ENVIRONMENT = get_string("OPEN_DISCUSSIONS_ENVIRONMENT", "dev")
 
