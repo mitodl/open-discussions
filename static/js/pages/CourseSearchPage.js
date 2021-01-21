b9e6c966b22582d18021da944142b071248279b1
Matt Bertrand <mrbertrand@gmail.com>
2021-01-21 14:32:10 -0500

    Ignore case, quotes, special chars when comparing suggestion to search text

--- static/js/pages/CourseSearchPage.js
+++ static/js/pages/CourseSearchPage.js
@@ -232,7 +232,16 @@ export default function CourseSearchPage(props: Props) {
   const resultsColumnWidth = searchResultLayout === SEARCH_GRID_UI ? 9 : 8
   const suggestions =
     !emptyOrNil(suggest) && !emptyOrNil(text)
-      ? R.without([text], suggest).map(
+      ? R.without(
+        [
+          text
+            .toLowerCase()
+            .replace(/^"(.*)"$/, "$1")
+            .replace(/[\W]+/g, " ")
+            .trim()
+        ],
+        suggest
+      ).map(
         suggestion => (isDoubleQuoted(text) ? `"${suggestion}"` : suggestion)
       )
       : []
