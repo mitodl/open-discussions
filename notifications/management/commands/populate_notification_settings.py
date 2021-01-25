c165f8d5db008fa287f7a8f8f16cbd12c106f5af
abeglova <abeglova@mit.edu>
2021-01-25 14:43:20 -0500

    fix inactive setting

--- notifications/management/commands/populate_notification_settings.py
+++ notifications/management/commands/populate_notification_settings.py
@@ -12,7 +12,7 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument("--username", nargs="?", action="append")
-        parser.add_argument("--inactive", action="store_false", default=False)
+        parser.add_argument("--inactive", action="store_true", default=False)
 
     def handle(self, *args, **options):
         users = get_user_model().objects.all()
