e658d1749c8e44a1703a6131f61c733b8f4d55a4
Chris Chudzicki <christopher.chudzicki@gmail.com>
2022-06-25 16:24:48 -0400

    set up util-components package; move CanonicalLink there

--- frontends/infinite-corridor/src/entry/root.tsx
+++ frontends/infinite-corridor/src/entry/root.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
+import { dog } from "util-components"
 
 const container = document.getElementById('container')!
 const root = createRoot(container)
