a0391441ff9d04074a0dde8980b926b2d5dc8321
Chris Chudzicki <christopher.chudzicki@gmail.com>
2022-06-29 18:16:03 -0400

    conditionally enable infinite corridor

--- infinite_example/urls.py
+++ infinite_example/urls.py
@@ -1,5 +1,9 @@
 """URL configuration for infinite_example"""
 from django.conf.urls import url
 from infinite_example.views import index
+from django.conf import settings
 
-urlpatterns = [url(r"^infinite", index)]
+print("ENABLE_INFINITE_CORRIDOR")
+print(settings.ENABLE_INFINITE_CORRIDOR)
+
+urlpatterns = [url(r"^infinite", index)] if settings.ENABLE_INFINITE_CORRIDOR else []
