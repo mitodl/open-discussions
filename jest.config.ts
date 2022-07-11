5546873715c1969614587fb0a649acccbe2bef7f
Chris Chudzicki <christopher.chudzicki@gmail.com>
2022-07-11 18:12:52 -0400

    restore jest.config.test, oops

---
+++ jest.config.ts
@@ -0,0 +1,9 @@
+import type { Config } from '@jest/types'
+
+const config: Config.InitialOptions = {
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  projects: ["<rootDir>/frontends/*/"]
+}
+
+export default config
