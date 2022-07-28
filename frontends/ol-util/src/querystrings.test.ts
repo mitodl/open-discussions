2ad5d01d5eb1b156313cd4e27df472c1e46a2897
Chris Chudzicki <cchudzicki@Chriss-MacBook-Pro.local>
2022-07-28 19:14:10 -0400

    fix tests

--- frontends/ol-util/src/querystrings.test.ts
+++ frontends/ol-util/src/querystrings.test.ts
@@ -5,12 +5,12 @@ describe("toQueryString", () => {
   })
 
   it.each([
-    { def: 1, abc: 10 },
+    { def: 10, abc: 1 },
     { abc: 1, def: 10 }
   ])(
     "should return a query string with params sorted lexicographically",
     params => {
-      expect(toQueryString(params)).toBe("abc=10&def=1")
+      expect(toQueryString(params)).toBe("abc=1&def=10")
     }
   )
 })
