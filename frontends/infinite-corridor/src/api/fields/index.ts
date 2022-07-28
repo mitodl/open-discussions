a5402482bec6fbaa656779058b40e1453a44066c
Chris Chudzicki <cchudzicki@Chriss-MacBook-Pro.local>
2022-07-28 18:16:36 -0400

    revert userlist stuff, save for another pr

--- frontends/infinite-corridor/src/api/fields/index.ts
+++ frontends/infinite-corridor/src/api/fields/index.ts
@@ -1,38 +1,14 @@
 import { useQuery } from "react-query"
-import type { UseQueryResult } from "react-query"
-import type {
-  Field,
-  PaginatedFields,
-  PaginatedUserListItems,
-  PaginatedFieldListItems
-} from "./interfaces"
+import { Field, FieldList } from "./interfaces"
 import * as urls from "./urls"
-import { useMemo } from "react"
 
 const useFieldsList = () => {
-  return useQuery<PaginatedFields>(urls.fieldsList)
+  return useQuery<FieldList>(urls.fieldsList)
 }
 
 const useFieldDetails = (name: string) => {
   return useQuery<Field>(urls.fieldDetails(name))
 }
 
-const useFieldListItems = (listId: number) => {
-  const userListItems = useQuery<PaginatedUserListItems>(
-    urls.userListItems(listId)
-  )
-  return useMemo(() => {
-    const { data, ...others } = userListItems
-    const lrData = data && {
-      ...data,
-      results: data.results.map(d => d.content_data)
-    }
-    return {
-      data: lrData,
-      ...others
-    } as UseQueryResult<PaginatedFieldListItems>
-  }, [userListItems])
-}
-
-export { useFieldsList, useFieldDetails, useFieldListItems, urls }
-export type { Field, PaginatedUserListItems }
+export { useFieldsList, useFieldDetails, urls }
+export type { Field, FieldList }
--- frontends/infinite-corridor/src/api/fields/interfaces.ts
+++ frontends/infinite-corridor/src/api/fields/interfaces.ts
@@ -1,27 +1,11 @@
 import type { PaginatedResult } from "ol-util"
-import type { UserListItem, UserList } from "ol-search-ui"
 
-interface Field {
+export interface Field {
   title: string
   name: string
   banner: string | null
-  featured_list: UserList
-  lists: UserList[]
   avatar_small: string | null
   avatar_medium: string | null
   public_description: string
 }
-
-type PaginatedFields = PaginatedResult<Field>
-
-type PaginatedUserListItems = PaginatedResult<UserListItem>
-
-type PaginatedFieldListItems = PaginatedResult<UserListItem["content_data"]>
-
-export {
-  Field,
-  PaginatedFields,
-  UserListItem,
-  PaginatedUserListItems,
-  PaginatedFieldListItems
-}
+export type FieldList = PaginatedResult<Field>
