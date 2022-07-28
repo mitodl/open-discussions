9e6b8d61394b1bbc13865b58b58bff00b1c2519d
Chris Chudzicki <cchudzicki@Chriss-MacBook-Pro.local>
2022-07-28 19:30:52 -0400

    revert another userlist change

--- frontends/infinite-corridor/src/api/fields/urls.ts
+++ frontends/infinite-corridor/src/api/fields/urls.ts
@@ -1,16 +1,3 @@
-import type { PaginationSearchParams } from "ol-util"
-import { toQueryString } from "ol-util"
-
 export const fieldsList = "/fields"
 
 export const fieldDetails = (name: string) => `/fields/${name}`
-
-const DEFAULT_PAGE_SIZE = 50
-
-export const userListItems = (
-  id: number,
-  { limit = DEFAULT_PAGE_SIZE, offset = 0 }: PaginationSearchParams = {}
-) => {
-  const searchParams = toQueryString({ limit, offset })
-  return `/userlists/${id}/items?${searchParams}`
-}
