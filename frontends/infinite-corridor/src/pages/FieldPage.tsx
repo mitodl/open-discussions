c33709c405094588b8bbbd579e5a5119b0f06561
Chris Chudzicki <christopher.chudzicki@gmail.com>
2022-08-04 14:24:09 -0400

    field page alignment

--- frontends/infinite-corridor/src/pages/FieldPage.tsx
+++ frontends/infinite-corridor/src/pages/FieldPage.tsx
@@ -125,17 +125,16 @@ const FieldPage: React.FC = () => {
         <Container>
           <Grid container spacing={1}>
             <Grid item xs={12} sm={9}>
-              <TabPanel value="home">
+              <TabPanel value="home" className="page-nav-content">
                 <p>{fieldQuery.data?.public_description}</p>
                 {featuredList && <FieldCarousel list={featuredList} />}
                 {fieldLists.map(list => (
                   <FieldList key={list.id} list={list} />
                 ))}
               </TabPanel>
-              <TabPanel value="about">BBBBBB</TabPanel>
-            </Grid>
-            <Grid item xs={12} sm={3}>
-              Featured Video
+              <TabPanel value="about" className="page-nav-content">
+              At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.
+              </TabPanel>
             </Grid>
           </Grid>
         </Container>
