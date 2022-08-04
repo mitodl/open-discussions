cab46c128ccf2519ec66f3ff58ededbdd042c305
Chris Chudzicki <christopher.chudzicki@gmail.com>
2022-08-04 14:50:39 -0400

    make header infinite a link

--- frontends/infinite-corridor/src/components/Header.tsx
+++ frontends/infinite-corridor/src/components/Header.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import AppBar from "@mui/material/AppBar"
 import Toolbar from "@mui/material/Toolbar"
 import Divider from "@mui/material/Divider"
+import { Link } from "react-router-dom"
 import { MITLogoLink } from "ol-util"
 
 const Header: React.FC = () => {
@@ -10,7 +11,9 @@ const Header: React.FC = () => {
       <Toolbar variant="dense">
         <MITLogoLink className="ic-mit-logo" />
         <Divider className="ic-divider" orientation="vertical" flexItem />
-        <h2>Infinite Corridor</h2>
+        <h2 className="ic-header-title">
+          <Link to="/infinite">Infinite Corridor</Link>
+        </h2>
       </Toolbar>
     </AppBar>
   )
