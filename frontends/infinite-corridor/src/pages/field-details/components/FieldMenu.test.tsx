225daa9e3becfa016db06b45a888340525033715
Chris Chudzicki <christopher.chudzicki@gmail.com>
2022-08-19 13:32:28 -0400

    add manage/widgets to fieldmenu test

--- frontends/infinite-corridor/src/pages/field-details/components/FieldMenu.test.tsx
+++ frontends/infinite-corridor/src/pages/field-details/components/FieldMenu.test.tsx
@@ -1,15 +1,16 @@
 import React from "react"
 import { Router } from "react-router"
 import { createMemoryHistory } from "history"
-import { render, screen } from "@testing-library/react"
+import { render, screen, waitFor } from "@testing-library/react"
 
 import { urls } from "../../../api/fields"
 import * as factories from "../../../api/fields/factories"
 import { setMockResponse, user } from "../../../test-utils"
 import FieldMenu from "./FieldMenu"
+import { assertInstanceOf } from "ol-util"
 
 describe("FieldMenu", () => {
-  it("Includes a link to the FieldChannel edit page", async () => {
+  it("Includes links to field management and widget management", async () => {
     const field = factories.makeField()
     setMockResponse.get(urls.fieldDetails(field.name), field)
     const history = createMemoryHistory()
@@ -20,7 +21,16 @@ describe("FieldMenu", () => {
     )
     const dropdown = await screen.findByText("settings")
     await user.click(dropdown)
-    const link = (await screen.findByRole("link")) as HTMLLinkElement
-    expect(link.href).toContain(`/infinite/fields/${field.name}/manage/`)
+    const links = await waitFor(async () => {
+      const found = await screen.findAllByRole("link")
+      expect(found.length).toBe(2)
+      found.every(link => assertInstanceOf(link, HTMLAnchorElement))
+      return found as HTMLAnchorElement[]
+    })
+
+    expect(links[0].href).toContain(`/infinite/fields/${field.name}/manage/`)
+    expect(links[1].href).toContain(
+      `/infinite/fields/${field.name}/manage/widgets/`
+    )
   })
 })
--- frontends/infinite-corridor/src/pages/urls.ts
+++ frontends/infinite-corridor/src/pages/urls.ts
@@ -6,7 +6,8 @@ export const HOME = BASE
 
 export const FIELD_VIEW = `${BASE}/fields/:name/` as const
 export const FIELD_EDIT = `${BASE}/fields/:name/manage/` as const
-export const FIELD_EDIT_WIDGETS = `${BASE}/fields/:name/manage/widgets` as const
+export const FIELD_EDIT_WIDGETS =
+  `${BASE}/fields/:name/manage/widgets/` as const
 export const makeFieldViewPath = (name: string) =>
   generatePath(FIELD_VIEW, { name })
 export const makeFieldEditPath = (name: string) =>
