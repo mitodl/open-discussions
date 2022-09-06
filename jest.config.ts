735d2ddd3a09ec7f3e1be17f0bad3cef83cca411
Chris Chudzicki <christopher.chudzicki@gmail.com>
2022-09-06 11:16:18 -0400

    add jest-watch-typeahead (#3723)

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,10 @@ const config: Config.InitialOptions = {
   collectCoverage:   true,
   coverageDirectory: "coverage",
   projects:          ["<rootDir>/frontends/*/"],
+  watchPlugins:      [
+    "jest-watch-typeahead/filename",
+    "jest-watch-typeahead/testname"
+  ]
 }
 
 export default config
