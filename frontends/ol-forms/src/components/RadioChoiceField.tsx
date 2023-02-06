28b2f17cdcc70f6e05e5dc220da24a1d7d9152fd
Chris Chudzicki <christopher.chudzicki@gmail.com>
2023-02-06 13:44:36 -0500

    add RadioChoiceField

---
+++ frontends/ol-forms/src/components/RadioChoiceField.tsx
@@ -0,0 +1,72 @@
+import React from "react"
+import RadioGroup, { RadioGroupProps } from "@mui/material/RadioGroup"
+import Radio from "@mui/material/Radio"
+import FormControlLabel from "@mui/material/FormControlLabel"
+import FormControl from "@mui/material/FormControl"
+import FormLabel from "@mui/material/FormLabel"
+import { useId } from "ol-util"
+
+interface RadioChoiceProps {
+  value: string
+  label: React.ReactNode
+  className?: string
+}
+
+interface RadioChoiceFieldProps {
+  label: string
+  value?: string
+  defaultValue?: string
+  name: string
+  choices: RadioChoiceProps[]
+  row?: boolean,
+  onChange?: RadioGroupProps["onChange"]
+}
+
+/**
+ * Wrapper around MUI components to make a form field with:
+ *  - radio group input
+ *  - label
+ *  - help text and error message, if any
+ *
+ * Avoid using MUI's Radio and RadioGroup directly. Prefer this component.
+ */
+const RadioChoiceField: React.FC<RadioChoiceFieldProps> = ({
+  label,
+  value,
+  row,
+  defaultValue,
+  name,
+  choices,
+  onChange
+}) => {
+  const labelId = useId()
+  return (
+    <FormControl>
+      <FormLabel id={labelId}>{label}</FormLabel>
+      <RadioGroup
+        aria-labelledby={labelId}
+        name={name}
+        defaultValue={defaultValue}
+        row={row}
+        value={value}
+        onChange={onChange}
+      >
+        {choices.map(choice => {
+          const { value, label, className } = choice
+          return (
+            <FormControlLabel
+              key={value}
+              value={value}
+              control={<Radio />}
+              label={label}
+              className={className}
+            />
+          )
+        })
+        }
+      </RadioGroup>
+    </FormControl>
+  )
+}
+
+export default RadioChoiceField
--- frontends/ol-forms/src/components/index.ts
+++ frontends/ol-forms/src/components/index.ts
@@ -1,2 +1,3 @@
 export { default as ValidationError } from "./ValidationError"
 export { default as SortableSelect } from "./SortableSelect"
+export { default as RadioChoiceField } from "./RadioChoiceField"
