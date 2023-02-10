c96debc4fd44ae44af75808d4557126a1dc9c8fa
Chris Chudzicki <christopher.chudzicki@gmail.com>
2023-02-10 16:41:23 -0500

    simplify useToggle

--- frontends/ol-util/src/hooks/useToggle.ts
+++ frontends/ol-util/src/hooks/useToggle.ts
@@ -1,5 +1,5 @@
 import type { Dispatch, SetStateAction } from "react"
-import { useCallback, useMemo, useState } from "react"
+import { useMemo, useState } from "react"
 
 interface Toggler extends Dispatch<SetStateAction<boolean>> {
   toggle: () => void
@@ -14,19 +14,14 @@ interface Toggler extends Dispatch<SetStateAction<boolean>> {
  */
 const useToggle = (initialValue: boolean): [boolean, Toggler] => {
   const [value, setValue] = useState(initialValue)
-  const toggle = useCallback(() => setValue(v => !v), [])
-  const on = useCallback(() => setValue(true), [])
-  const off = useCallback(() => setValue(false), [])
 
   const toggler: Toggler = useMemo(() => {
     return Object.assign(setValue, {
-      on,
-      off,
-      toggle,
-      set: setValue
+      on:     () => setValue(true),
+      off:    () => setValue(false),
+      toggle: () => setValue(current => !current),
+      set:    setValue
     })
-    // This is safe because none of the above have any dependencies
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   return [value, toggler]
