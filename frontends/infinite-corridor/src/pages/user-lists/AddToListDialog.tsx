8107a2445c64ba92e9ed7b5e4bcf2933bd9bbed4
Chris Chudzicki <christopher.chudzicki@gmail.com>
2023-02-22 09:47:32 -0500

    add spinner, disabled checkboxes

--- frontends/infinite-corridor/src/pages/user-lists/AddToListDialog.tsx
+++ frontends/infinite-corridor/src/pages/user-lists/AddToListDialog.tsx
@@ -5,18 +5,18 @@ import CloseIcon from "@mui/icons-material/Close"
 import IconButton from "@mui/material/IconButton"
 import DialogContent from "@mui/material/DialogContent"
 import DialogTitle from "@mui/material/DialogTitle"
-import { LearningResource, PrivacyLevel, UserList } from "ol-search-ui"
 import Divider from "@mui/material/Divider"
 import Checkbox from "@mui/material/Checkbox"
-
 import List from '@mui/material/List'
 import ListItem from '@mui/material/ListItem'
 import ListItemButton from '@mui/material/ListItemButton'
-import ListItemIcon from '@mui/material/ListItemIcon'
 import ListItemText from '@mui/material/ListItemText'
 import LockOpenIcon from '@mui/icons-material/LockOpen'
 import LockIcon from '@mui/icons-material/Lock'
 import Chip from "@mui/material/Chip"
+
+import { LearningResource, PrivacyLevel, UserList } from "ol-search-ui"
+import { LoadingSpinner } from "ol-util"
 
 import { useAddToUserListItems, useDeleteFromUserListItems, useResource, useUserListsListing } from "../../api/learning-resources"
 
@@ -28,29 +28,55 @@ type AddToListDialogProps =  {
   onClose: () => void
 }
 
+const useRequestRecord = () => {
+  const [pending, setPending] = useState<Map<string, "delete" | "add">>(new Map())
+  const key = (resource: LearningResource, userList: UserList) => `${resource.object_type}-${resource.id}-${userList.id}`
+  const get = (resource: LearningResource, userList: UserList) => pending.get(key(resource, userList))
+  const set = (resource: LearningResource, userList: UserList, value: "delete" | "add") => {
+    setPending(current => new Map(current).set(key(resource, userList), value))
+  }
+  const clear = (resource: LearningResource, userList: UserList) => {
+    setPending(current => {
+      const next = new Map(current)
+      next.delete(key(resource, userList))
+      return next
+    })
+  }
+  return { get, set, clear }
+}
+
 const AddToListDialog: React.FC<AddToListDialogProps> = ({
   open,
   resourceKey,
   onClose
 }) => {
+  const requestRecord = useRequestRecord()
   const resourceQuery = useResource(resourceKey.object_type, resourceKey.id)
   const resource = resourceQuery.data
   const userListsQuery = useUserListsListing()
+  const userLists = userListsQuery.data?.results
   const addTo = useAddToUserListItems()
   const deleteFrom = useDeleteFromUserListItems()
-
-  const handleToggle: (list: UserList) => () => void = userList => () => {
+  const handleToggle = (userList: UserList) => async () => {
     if (!resource) return
     const listItem = resource.lists.find(list => list.list_id === userList.id)
-    if (listItem) {
-      deleteFrom.mutate(listItem)
-    } else {
-      addTo.mutate({
-        userListId: userList.id,
-        payload:    { object_id: resource.id, content_type: resource.object_type }
-      })
+    try {
+      if (listItem) {
+        requestRecord.set(resource, userList, "delete")
+        await deleteFrom.mutateAsync(listItem)
+      } else {
+        requestRecord.set(resource, userList, "add")
+        await addTo.mutateAsync({
+          userListId: userList.id,
+          payload:    { object_id: resource.id, content_type: resource.object_type }
+        })
+      }
+    } finally {
+      requestRecord.clear(resource, userList)
     }
   }
+
+  const isReady = resource && userLists
 
   return (
     <Dialog className="add-to-list-dialog" open={open} onClose={onClose}>
@@ -61,19 +87,26 @@ const AddToListDialog: React.FC<AddToListDialogProps> = ({
         </IconButton>
       </Box>
       <Divider />
-      <DialogContent>
-        Adding <span className="resource-title-inline">{resource?.title}</span>
-        <List disablePadding>
+      {isReady && <DialogContent className="add-to-list-description">
+        Adding <span className="resource-title-inline">{resource.title}</span>
+      </DialogContent>
+      }
+      {isReady && <DialogContent className="add-to-list-listing">
+        <List>
           {userListsQuery?.data?.results.map(userList => {
             const labelId = `checkbox-list-label-${userList.id}`
-            const isChecked = resource?.lists.some(list => list.list_id === userList.id)
+            const isAdding = requestRecord.get(resource, userList) === "add"
+            const isDeleting = requestRecord.get(resource, userList) === "delete"
+            const disabled = isAdding || isDeleting
+            const isChecked = isAdding || resource.lists.some(list => list.list_id === userList.id)
             return  (
               <ListItem key={userList.id} secondaryAction={
                 <PrivacyChip privacyLevel={userList.privacy_level} />
               }>
-                <ListItemButton onClick={handleToggle(userList)}>
+                <ListItemButton disabled={disabled} onClick={handleToggle(userList)}>
                   <Checkbox
                     edge="start"
+                    disabled={disabled}
                     checked={isChecked}
                     tabIndex={-1}
                     disableRipple
@@ -85,7 +118,10 @@ const AddToListDialog: React.FC<AddToListDialogProps> = ({
             )
           })}
         </List>
-      </DialogContent>
+      </DialogContent>}
+      {!isReady && <DialogContent>
+        <LoadingSpinner loading={!isReady} />
+      </DialogContent>}
     </Dialog>
   )
 }
