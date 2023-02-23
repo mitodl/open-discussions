5a73645cccbc42bac006daa33a2d2a1b9029f472
Chris Chudzicki <christopher.chudzicki@gmail.com>
2023-02-23 13:11:18 -0500

    simplify rtl errors

--- jest-shared-setup.ts
+++ jest-shared-setup.ts
@@ -1,4 +1,22 @@
 import failOnConsole from "jest-fail-on-console"
 import '@testing-library/jest-dom'
+import { configure } from "@testing-library/dom"
 
 failOnConsole()
+
+configure({
+  /**
+   * Adapted from https://github.com/testing-library/dom-testing-library/issues/773
+   * to make the error messages a bit more succinct.
+   *
+   * By default, testing-library prints much too much of the DOM.
+   *
+   * This does change the stacktrace a bit: The line causing the error is still
+   * there, but the line where the error is generated (below) is most visible.
+   */
+  getElementError(message, _container) {
+    const error = new Error(message ?? "")
+    error.name = "TestingLibraryElementError"
+    return error
+  },
+})
