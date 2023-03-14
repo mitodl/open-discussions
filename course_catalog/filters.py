4cd438b285186d5f12c7f0c7ea8088834751d30a
Jenni Whitman <jenniw@mit.edu>
2023-03-14 12:04:21 -0400

    reformat

--- course_catalog/filters.py
+++ course_catalog/filters.py
@@ -1,14 +1,13 @@
 """Course Catalog Filters"""
 import django_filters
+
 from course_catalog.models import Course
 
 
 class CourseFilter(django_filters.FilterSet):
     """Course filter"""
 
-    upcoming = django_filters.DateFilter(
-        field_name="start_date", lookup_expr="gt"
-    )
+    upcoming = django_filters.DateFilter(field_name="start_date", lookup_expr="gt")
 
     certificate = django_filters.Filter(
         field_name="certificates", lookup_expr="icontains"
