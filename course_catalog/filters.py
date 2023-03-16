86b527acf5d9bb8fd6f247de29f21aaa9a63ae5e
Jenni Whitman <jenniw@mit.edu>
2023-03-16 15:06:23 -0400

    add in cert filter

--- course_catalog/filters.py
+++ course_catalog/filters.py
@@ -1,4 +1,5 @@
 """Course Catalog Filters for API"""
+from django.db.models import Max
 from django_filters import AllValuesFilter, ChoiceFilter, FilterSet
 
 from course_catalog.models import (
@@ -29,4 +30,31 @@ class CourseFilter(FilterSet):
             queryset = queryset.filter(platform__in=PROFESSIONAL_COURSE_PLATFORMS)
         else:
             queryset = queryset.exclude(platform__in=PROFESSIONAL_COURSE_PLATFORMS)
+        return queryset
+
+    def filter_certificate(self, queryset, _, value):
+        if value:
+            qs1 = (
+                queryset.filter(
+                    runs__availability__in=["Current", "Upcoming", "Starting Soon"]
+                )
+                .filter(platform="mtx")
+                .annotate(max_start_date=Max("runs__start_date"))
+            )
+            qs2 = queryset.filter(platform__in=PROFESSIONAL_COURSE_PLATFORMS).annotate(
+                max_start_date=Max("runs__start_date")
+            )
+            queryset = qs1.union(qs2)
+        else:
+            qs1 = (
+                queryset.filter(
+                    runs__availability__not_in=["Current", "Upcoming", "Starting Soon"]
+                )
+                .filter(platform="mtx")
+                .annotate(max_start_date=Max("runs__start_date"))
+            )
+            qs2 = queryset.filter(platform__in=PROFESSIONAL_COURSE_PLATFORMS).annotate(
+                max_start_date=Max("runs__start_date")
+            )
+            queryset = qs1.union(qs2)
         return queryset
