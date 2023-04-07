9a8767eab05ec908a0759c6c0055381c0c5844a7
jenniw <jenniw@mit.edu>
2023-04-07 12:29:47 -0400

    somehow lost the opensearch fixture, putting it back

---
+++ fixtures/opensearch.py
@@ -0,0 +1,17 @@
+"""Opensearch fixtures"""
+from types import SimpleNamespace
+
+import pytest
+
+from search.connection import configure_connections
+
+
+@pytest.fixture(autouse=True)
+def opensearch(mocker, settings):
+    """Fixture for mocking opensearch"""
+    settings.OPENSEARCH_URL = "test.open"
+    mock_get_connection = mocker.patch(
+        "opensearch_dsl.connections.Connections.get_connection", autospec=True
+    )
+    configure_connections()
+    yield SimpleNamespace(conn=mock_get_connection.return_value)
