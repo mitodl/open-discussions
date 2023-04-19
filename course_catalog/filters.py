c46660bff5a1ea6dd63e82cb5b420c0f412cbf8b
Jenni <JenniWhitman@users.noreply.github.com>
2023-04-19 15:40:40 -0400

    Correct issues with documentation for offered_by filter on courses endpoint (#3942)
    * serializer hold
    
    * docs match and function. extend doesn't extend.

--- course_catalog/filters.py
+++ course_catalog/filters.py
@@ -1,5 +1,7 @@
 """Course Catalog Filters for API"""
 from django_filters import ChoiceFilter, FilterSet
+from drf_spectacular.plumbing import build_choice_description_list
+from drf_spectacular.utils import extend_schema_field
 
 from course_catalog.constants import OfferedBy
 from course_catalog.models import PROFESSIONAL_COURSE_PLATFORMS, Course
@@ -17,8 +19,18 @@ class CourseFilter(FilterSet):
         lookup_expr="in",
         choices=(("professional", "professional"), ("open", "open")),
     )
-    offered_by = ChoiceFilter(
-        method="filter_offered_by", choices=OFFERED_BY_CHOICES, field_name="offered_by"
+    offered_by = extend_schema_field(
+        {
+            "type": "string",
+            "enum": [x for x, _ in OFFERED_BY_CHOICES],
+            "description": f"offered_by \n {build_choice_description_list(OFFERED_BY_CHOICES)}",
+        }
+    )(
+        ChoiceFilter(
+            method="filter_offered_by",
+            choices=OFFERED_BY_CHOICES,
+            field_name="offered_by",
+        )
     )
 
     class Meta:
