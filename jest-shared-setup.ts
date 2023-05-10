fc7166b63deef30c43fc53eb78e097a0f5b9550c
Chris Chudzicki <christopher.chudzicki@gmail.com>
2023-05-10 11:50:15 -0400

    add a comment

--- jest-shared-setup.ts
+++ jest-shared-setup.ts
@@ -3,6 +3,12 @@ import '@testing-library/jest-dom'
 import { configure } from "@testing-library/dom"
 
 failOnConsole({
+  /**
+   * This warning is removed from newer versions of React because it is
+   * misleading (not usually a memory leak).
+   *
+   * See https://github.com/reactwg/react-18/discussions/82
+   */
   silenceMessage: msg => /Can't perform a React state update on an unmounted component/.test(msg),
 })
 
