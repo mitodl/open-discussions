0a63b3c80c56abf62862799dd932eb0d4b2d68ad
Doof <mitx-devops@mit.edu>
2023-06-06 18:23:14 +0000

    Release 0.219.0

--- open_discussions/settings.py
+++ open_discussions/settings.py
@@ -32,7 +32,7 @@ from open_discussions.settings_celery import *
 from open_discussions.settings_course_etl import *
 from open_discussions.settings_spectacular import open_spectacular_settings
 
-VERSION = "0.218.1"
+VERSION = "0.219.0"
 
 log = logging.getLogger()
 
