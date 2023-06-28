7ccf4caa5a2757b379a353a9ccd80bea7a211609
jenniw <jenniw@mit.edu>
2023-06-28 08:15:12 -0400

    fmt

--- frontends/infinite-corridor/src/pages/ForbiddenPage.test.tsx
+++ frontends/infinite-corridor/src/pages/ForbiddenPage.test.tsx
@@ -3,7 +3,7 @@ import { renderWithProviders, screen, user } from "../test-utils"
 import ForbiddenPage from "./ForbiddenPage"
 import HomePage from "./HomePage"
 
-test("The ForbiddenPage loads with meta", async ()=>{
+test("The ForbiddenPage loads with meta", async () => {
   renderWithProviders(<ForbiddenPage />, {})
   const meta = document.querySelector('meta[name="robots"]')
   expect(meta).toBeInTheDocument()
@@ -11,12 +11,12 @@ test("The ForbiddenPage loads with meta", async ()=>{
 
 test("The ForbiddenPage loads with Correct Title", async () => {
   renderWithProviders(<ForbiddenPage />, {})
-  screen.getByRole("heading", {name: "403 Forbidden Error"})
+  screen.getByRole("heading", { name: "403 Forbidden Error" })
 })
 
 test("The ForbiddenPage loads with a button that directs to HomePage", async () => {
   renderWithProviders(<ForbiddenPage />, {})
-  const homeButton = screen.getByRole("button", {name: "Return Home"})
+  const homeButton = screen.getByRole("button", { name: "Return Home" })
   await user.click(homeButton)
   expect(HomePage).toHaveBeenCalled()
 })
--- frontends/infinite-corridor/src/pages/NotFoundPage.test.tsx
+++ frontends/infinite-corridor/src/pages/NotFoundPage.test.tsx
@@ -3,7 +3,7 @@ import { renderWithProviders, screen, user } from "../test-utils"
 import NotFoundPage from "./NotFoundPage"
 import HomePage from "./HomePage"
 
-test("The NotFoundPage loads with meta", ()=>{
+test("The NotFoundPage loads with meta", () => {
   renderWithProviders(<NotFoundPage />, {})
   const meta = document.querySelector('meta[name="robots"]')
   expect(meta).toBeInTheDocument()
@@ -11,12 +11,12 @@ test("The NotFoundPage loads with meta", ()=>{
 
 test("The NotFoundPage loads with Correct Title", () => {
   renderWithProviders(<NotFoundPage />, {})
-  screen.getByRole("heading", {name: "404 Resource Not Found"})
+  screen.getByRole("heading", { name: "404 Resource Not Found" })
 })
 
 test("The NotFoundPage loads with a button that directs to HomePage", async () => {
   renderWithProviders(<NotFoundPage />, {})
-  const homeButton = await screen.findByRole("button", {name: "Return Home"})
+  const homeButton = await screen.findByRole("button", { name: "Return Home" })
   await user.click(homeButton)
   expect(HomePage).toHaveBeenCalled()
 })
