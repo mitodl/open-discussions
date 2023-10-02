797d5b2d1aa2bbae0d617b8122faae1262e51687
Collin Preston <collin_preston@yahoo.com>
2023-10-02 14:24:23 -0400

    Remove unused import

--- open_discussions/urls.py
+++ open_discussions/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, re_path, reverse
+from django.urls import include, re_path
 from django.views.generic import RedirectView
 from rest_framework_jwt.views import refresh_jwt_token
 
