c13f779330b7500db8577855bdc94cb394a0c791
Collin Preston <collin_preston@yahoo.com>
2023-12-08 16:01:06 -0500

    Add module docstring

--- keycloak_user_export/models.py
+++ keycloak_user_export/models.py
@@ -1,10 +1,13 @@
+"""
+Link to user records that have been successfully exported to Keycloak.
+"""
 from django.db import models
 from django.conf import settings
 
 
 class UserExportToKeycloak(models.Model):
     """
-    Link user records and a boolean indicator of whether the user has been successfully exported to Keycloak.
+    Link to user records that have been successfully exported to Keycloak.
     """
 
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
