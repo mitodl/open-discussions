f45a116784ee40900eceeb15e2723aad3e15446a
Tobias Macey <tmacey@boundlessnotions.com>
2025-07-23 13:23:40 -0400

    config: Disable scheduled tasks that are no longer needed (#4381)
    A majority of the scheduled tasks in this application pertain to ETL tasks that have
    been migrated to Learn. There is no need to continue running them in this application.

--- open_discussions/settings_celery.py
+++ open_discussions/settings_celery.py
@@ -21,9 +21,9 @@ CELERY_BEAT_SCHEDULE = {
         "task": "channels.tasks.evict_expired_access_tokens",
         "schedule": crontab(minute=0, hour="*"),
     },
-    "send-unsent-emails-every-1-mins": {
+    "send-unsent-emails-four-times-daily": {
         "task": "notifications.tasks.send_unsent_email_notifications",
-        "schedule": crontab(minute="*"),
+        "schedule": crontab(minute=0, hour="*/6"),
     },
     "send-frontpage-digests-every-1-days": {
         "task": "notifications.tasks.send_daily_frontpage_digests",
@@ -33,65 +33,11 @@ CELERY_BEAT_SCHEDULE = {
         "task": "notifications.tasks.send_weekly_frontpage_digests",
         "schedule": crontab(minute=0, hour=14, day_of_week=2),  # 10am EST on tuesdays
     },
-    "update_edx-courses-every-1-days": {
-        "task": "course_catalog.tasks.get_mitx_data",
-        "schedule": crontab(minute=30, hour=15),  # 11:30am EST
-    },
-    "update-edx-files-every-1-weeks": {
-        "task": "course_catalog.tasks.import_all_mitx_files",
-        "schedule": crontab(
-            minute=0, hour=16, day_of_week=1
-        ),  # 12:00 PM EST on Mondays
-    },
-    "update-micromasters-courses-every-1-days": {
-        "task": "course_catalog.tasks.get_micromasters_data",
-        "schedule": crontab(minute=00, hour=15),  # 11:00am EST
-    },
     "update-podcasts": {
         "task": "course_catalog.tasks.get_podcast_data",
         "schedule": get_int(
             "PODCAST_FETCH_SCHEDULE_SECONDS", 60 * 60 * 2
         ),  # default is every 2 hours
-    },
-    "update-xpro-courses-every-1-days": {
-        "task": "course_catalog.tasks.get_xpro_data",
-        "schedule": crontab(minute=30, hour=17),  # 1:30pm EST
-    },
-    "update-xpro-files-every-1-weeks": {
-        "task": "course_catalog.tasks.import_all_xpro_files",
-        "schedule": crontab(
-            minute=0, hour=16, day_of_week=2
-        ),  # 12:00 PM EST on Tuesdays
-    },
-    "update-mitxonline-courses-every-1-days": {
-        "task": "course_catalog.tasks.get_mitxonline_data",
-        "schedule": crontab(minute=30, hour=19),  # 3:30pm EST
-    },
-    "update-mitxonline-files-every-1-weeks": {
-        "task": "course_catalog.tasks.import_all_mitxonline_files",
-        "schedule": crontab(
-            minute=0, hour=16, day_of_week=3
-        ),  # 12:00 PM EST on Wednesdays
-    },
-    "update-oll-courses-every-1-days": {
-        "task": "course_catalog.tasks.get_oll_data",
-        "schedule": crontab(minute=30, hour=18),  # 2:30pm EST
-    },
-    "update-prolearn-courses-every-1-days": {
-        "task": "course_catalog.tasks.get_prolearn_data",
-        "schedule": crontab(minute=30, hour=21),  # 5:30pm EST
-    },
-    "update-youtube-videos": {
-        "task": "course_catalog.tasks.get_youtube_data",
-        "schedule": get_int(
-            "YOUTUBE_FETCH_SCHEDULE_SECONDS", 60 * 30
-        ),  # default is every 30 minutes
-    },
-    "update-youtube-transcripts": {
-        "task": "course_catalog.tasks.get_youtube_transcripts",
-        "schedule": get_int(
-            "YOUTUBE_FETCH_TRANSCRIPT_SCHEDULE_SECONDS", 60 * 60 * 12
-        ),  # default is 12 hours
     },
     "update-managed-channel-memberships": {
         "task": "channels.tasks.update_memberships_for_managed_channels",
